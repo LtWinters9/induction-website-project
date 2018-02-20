eb69ff89e940c8b3c712ed075280b7f12c84da68
smunsie <35263623+smunsie@users.noreply.github.com>
2018-02-20 13:37:46 +0000

    Add files via upload

---
+++ Database scripts/Insert Statements.sql
@@ -0,0 +1,85 @@
+insert into college (collegeid,collegename,description,addressline1,town,postcode) VALUES ('PC01','Perth College','Our picturesque, leafy college on the outskirts of the city is a vibrant community of more than 9,000 students and 650 teaching and support staff.','Crieff Rd','Perth','PH1 2NX');
+insert into college (collegeid,collegename,description,addressline1,town,postcode) VALUES ('IV01','Inverness College','Inverness College UHI is one of the largest partners of the University of the Highlands and Islands (UHI).   Through a partnership with 12 other colleges and internationally renowned research centres, we are making it easier for people to study high quality further and higher education across the region. Our vast selection of courses, from national certificates to post graduate degrees, are taught to over 6,500 full-time and part-time students','1 Inverness Campus','Inverness','IV2 5NA');
+
+
+insert into location (locationid,locationname,locationdes,collegeid) values ('BB01','Brahan Building','Brahan building is the largest building at Perth College. It houses many different departments as well as a cafetria, coffee shop and snack shop.','PC01');
+insert into location (locationid,locationname,locationdes,collegeid) values ('GB01','Goodlyburn Building','Goodlyburn building is a building that houses various wonderful things','PC01');
+insert into location (locationid,locationname,locationdes,collegeid) values ('ASW01','Academy of Sport and Wellbeing','The Academy of Sport and Wellbeing is our brand new sport centre that has everything you need to get healthy','PC01');
+insert into location (locationid,locationname,locationdes,collegeid) values ('WB01','Webster Building and Union-Link','Perth Colleges union resides here where you can chill out with your coursemates and drink coffee.(NO BEER AVAILABLE)','PC01');
+insert into location (locationid,locationname,locationdes,collegeid) values ('DAE01','Dunne Aeronautical Laboratory','We are well known for our aeronatuical laboratory and here you will discovery all you need to know about aeroplane engineering','PC01');
+insert into location (locationid,locationname,locationdes,collegeid) values ('ICP01','Inverness Campus','Our InvernessCampus is the main campus and is where the majority of our courses are delivered.','IV01');
+insert into location (locationid,locationname,locationdes,collegeid) values ('SSF01','Scottish School of Forestry','The stunning forested scenery of Viewhill, Balloch, is just seven miles from Inverness city centre. It is here, on a self-contained site, that our Scottish School of Forestry is based','IV01');
+
+insert into course (courseid,coursename,coursedes) values ('HNCCom','HNC Computing','Learn all there is to know about the computing world.');
+insert into course (courseid,coursename,coursedes) values ('HNDCom','HND Computer Science','Advance your skills and knowledge with computers and advance your coding skills.');
+insert into course (courseid,coursename,coursedes) values ('HNCHD','HNC Hairdressing','Lear how to cut hair');
+insert into course (courseid,coursename,coursedes) values ('HNCBA','HNC Business and Accounting','Learn all there is to know about Business and Accounting');
+insert into course (courseid,coursename,coursedes) values ('HNCSF','HNC Sport and Fitness','Learn all there is to know about sport and fitness');
+insert into course (courseid,coursename,coursedes) values ('HNDMgmt','HND Management','Advance your skills in management');
+
+
+
+insert into users (userid,forename,surname,dob,telephone,addressline1,town,postcode,email,bio,usertype,courseid,collegeid) values (15011406,'Scott','Munsie','1993-07-28','07749056381','6 Drive Drive','Perth','PH1 9TY','15011406@uhi.ac.uk',null,2,'HNCCom','PC01');
+insert into users (userid,forename,surname,dob,telephone,addressline1,town,postcode,email,bio,usertype,courseid,collegeid) values (083321,'David','Ferguson','1993-07-28','07749056381','6 Drive Drive','Perth','PH1 9TY','083321@uhi.ac.uk',null,2,'HNDCom','PC01');
+insert into users (userid,forename,surname,dob,telephone,addressline1,town,postcode,email,bio,usertype,courseid,collegeid) values (09012233,'Scott','Robertson','1993-07-28','07749056381','6 Drive Drive','Perth','PH1 9TY','09012233@uhi.ac.uk',null,2,'HNCHD','PC01');
+insert into users (userid,forename,surname,dob,telephone,addressline1,town,postcode,email,bio,usertype,courseid,collegeid) values (13004417,'Benjamin','Traill','1993-07-28','07749056381','6 Drive Drive','Perth','PH1 9TY','13004417@uhi.ac.uk',null,2,'HNCSF','PC01');
+insert into users (userid,forename,surname,dob,telephone,addressline1,town,postcode,email,bio,usertype,courseid,collegeid) values (65231354,'James','Jamieson','1993-07-28','07749056381','6 Drive Drive','Perth','PH1 9TY','65231354@uhi.ac.uk',null,2,'HNCCom','IV01');
+insert into users (userid,forename,surname,dob,telephone,addressline1,town,postcode,email,bio,usertype,courseid,collegeid) values (12345678,'Heather','Smith','1993-07-28','07749056381','6 Drive Drive','Perth','PH1 9TY','12345678@uhi.ac.uk',null,2,'HNDMgmt','IV01');
+
+insert into users (userid,forename,surname,dob,email,bio,usertype,courseid,collegeid) values (123,'Derek','Summers',null,'Derek@uhi.ac.uk','Derek is a great guy',3,'HNCCom','PC01');
+insert into users (userid,forename,surname,dob,email,bio,usertype,courseid,collegeid) values (234,'Ian','Turnbull',null,'Ian@uhi.ac.uk','Ian is a great guy',3,'HNCCom','PC01');
+insert into users (userid,forename,surname,dob,email,bio,usertype,courseid,collegeid) values (345,'Mike','Kula',null,'Mike@uhi.ac.uk','Mike is a great guy',3,'HNCCom','PC01');
+insert into users (userid,forename,surname,dob,email,bio,usertype,courseid,collegeid) values (456,'Gillian','Douglas',null,'Gillian@uhi.ac.uk','Gillian is a great guy',3,'HNCCom','PC01');
+insert into users (userid,forename,surname,dob,email,bio,usertype,courseid,collegeid) values (567,'Craig','Smith',null,'Craig@uhi.ac.uk','Craig is a great guy',3,'HNCCom','IV01');
+
+insert into userpass (passwordid,userpass,salt,userid) values (1,'ad98e978a7c2921ab4c3ab2194445c544f51d412963d7ab6d61c3c9a48f418db','2a8a23ab94ab1695f09ce4a622b742e5',15011406);
+insert into userpass (passwordid,userpass,salt,userid) values (2,'ad98e978a7c2921ab4c3ab2194445c544f51d412963d7ab6d61c3c9a48f418db','2a8a23ab94ab1695f09ce4a622b742e5',083321);
+insert into userpass (passwordid,userpass,salt,userid) values (3,'ad98e978a7c2921ab4c3ab2194445c544f51d412963d7ab6d61c3c9a48f418db','2a8a23ab94ab1695f09ce4a622b742e5',09012233);
+insert into userpass (passwordid,userpass,salt,userid) values (4,'ad98e978a7c2921ab4c3ab2194445c544f51d412963d7ab6d61c3c9a48f418db','2a8a23ab94ab1695f09ce4a622b742e5',13004417);
+insert into userpass (passwordid,userpass,salt,userid) values (5,'ad98e978a7c2921ab4c3ab2194445c544f51d412963d7ab6d61c3c9a48f418db','2a8a23ab94ab1695f09ce4a622b742e5',65231354);
+insert into userpass (passwordid,userpass,salt,userid) values (6,'ad98e978a7c2921ab4c3ab2194445c544f51d412963d7ab6d61c3c9a48f418db','2a8a23ab94ab1695f09ce4a622b742e5',12345678);
+insert into userpass (passwordid,userpass,salt,userid) values (7,'ad98e978a7c2921ab4c3ab2194445c544f51d412963d7ab6d61c3c9a48f418db','2a8a23ab94ab1695f09ce4a622b742e5',123);
+insert into userpass (passwordid,userpass,salt,userid) values (8,'ad98e978a7c2921ab4c3ab2194445c544f51d412963d7ab6d61c3c9a48f418db','2a8a23ab94ab1695f09ce4a622b742e5',234);
+insert into userpass (passwordid,userpass,salt,userid) values (9,'ad98e978a7c2921ab4c3ab2194445c544f51d412963d7ab6d61c3c9a48f418db','2a8a23ab94ab1695f09ce4a622b742e5',345);
+insert into userpass (passwordid,userpass,salt,userid) values (10,'ad98e978a7c2921ab4c3ab2194445c544f51d412963d7ab6d61c3c9a48f418db','2a8a23ab94ab1695f09ce4a622b742e5',456);
+insert into userpass (passwordid,userpass,salt,userid) values (11,'ad98e978a7c2921ab4c3ab2194445c544f51d412963d7ab6d61c3c9a48f418db','2a8a23ab94ab1695f09ce4a622b742e5',234);
+
+
+
+
+insert into module (moduleid,modulename,moduledes,courseid) values ('TW01','Team Working in Computing','Learn how to work as a team','HNCCom');
+insert into module (moduleid,modulename,moduledes,courseid) values ('SQL01','SQL: Introduction','Learn how to create SQL databases','HNCCom');
+insert into module (moduleid,modulename,moduledes,courseid) values ('DS01','Developing Software: Introduction','Learn how to code','HNCCom');
+insert into module (moduleid,modulename,moduledes,courseid) values ('GU01','Graded Unit 1','Examination on what you have learned','HNCCom');
+
+
+insert into mainblog (title,blogtext,blogtime,userposter,collegeid) values ('Main Blog Entry Number 1 Heading','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue a urna nec pharetra. Maecenas et felis ipsum. Donec mattis lacus orci, quis hendrerit nibh blandit at.','2014-04-14 23:11:11',15011406,'PC01');
+insert into mainblog (title,blogtext,blogtime,userposter,collegeid) values ('Main Blog Entry Number 2 Heading','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue a urna nec pharetra. Maecenas et felis ipsum. Donec mattis lacus orci, quis hendrerit nibh blandit at.','2014-04-14 23:11:11',083321,'PC01');
+insert into mainblog (title,blogtext,blogtime,userposter,collegeid) values ('Main Blog Entry Number 3 Heading','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue a urna nec pharetra. Maecenas et felis ipsum. Donec mattis lacus orci, quis hendrerit nibh blandit at.','2014-04-14 23:11:11',15011406,'PC01');
+insert into mainblog (title,blogtext,blogtime,userposter,collegeid) values ('Main Blog Entry Number 4 Heading','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue a urna nec pharetra. Maecenas et felis ipsum. Donec mattis lacus orci, quis hendrerit nibh blandit at.','2014-04-14 23:11:11',083321,'PC01');
+
+
+insert into mainblogcom (commenttext,blogtime,userid,mainblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,1);
+insert into mainblogcom (commenttext,blogtime,userid,mainblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,2);
+insert into mainblogcom (commenttext,blogtime,userid,mainblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,3);
+insert into mainblogcom (commenttext,blogtime,userid,mainblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,1);
+insert into mainblogcom (commenttext,blogtime,userid,mainblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,2);
+insert into mainblogcom (commenttext,blogtime,userid,mainblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,4);
+insert into mainblogcom (commenttext,blogtime,userid,mainblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,3);
+
+
+
+insert into courseblog (title,blogtext,blogtime,userposter,courseid,collegeid) values ('Course Blog Entry Number 1 Heading','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue a urna nec pharetra. Maecenas et felis ipsum. Donec mattis lacus orci, quis hendrerit nibh blandit at.','2014-04-14 23:11:11',15011406,'HNCCom','PC01');
+insert into courseblog (title,blogtext,blogtime,userposter,courseid,collegeid) values ('Course Blog Entry Number 2 Heading','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue a urna nec pharetra. Maecenas et felis ipsum. Donec mattis lacus orci, quis hendrerit nibh blandit at.','2014-04-14 23:11:11',083321,'HNCCom','PC01');
+insert into courseblog (title,blogtext,blogtime,userposter,courseid,collegeid) values ('Course Blog Entry Number 3 Heading','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue a urna nec pharetra. Maecenas et felis ipsum. Donec mattis lacus orci, quis hendrerit nibh blandit at.','2014-04-14 23:11:11',15011406,'HNDCom','PC01');
+insert into courseblog (title,blogtext,blogtime,userposter,courseid,collegeid) values ('Course Blog Entry Number 4 Heading','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla congue a urna nec pharetra. Maecenas et felis ipsum. Donec mattis lacus orci, quis hendrerit nibh blandit at.','2014-04-14 23:11:11',083321,'HNDCom','PC01');
+
+
+insert into courseblogcom (commenttext,blogtime,userid,courseblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,1);
+insert into courseblogcom (commenttext,blogtime,userid,courseblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,2);
+insert into courseblogcom (commenttext,blogtime,userid,courseblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,3);
+insert into courseblogcom (commenttext,blogtime,userid,courseblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,1);
+insert into courseblogcom (commenttext,blogtime,userid,courseblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,2);
+insert into courseblogcom (commenttext,blogtime,userid,courseblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,4);
+insert into courseblogcom (commenttext,blogtime,userid,courseblogid) values ('Lorem ipsum dolor sit amet, consectetur adipiscing elit.','2014-04-14 23:11:11',09012233,3);
+
---
+++ Database scripts/final script.sql
@@ -0,0 +1,131 @@
+drop table if exists courseblogcom;
+drop table if exists courseblog;
+drop table if exists mainblogcom;
+drop table if exists mainblog;
+drop table if exists module;
+drop table if exists userpass;
+drop table if exists users;
+drop table if exists course;
+drop table if exists location;
+drop table if exists college;
+
+
+
+create table college (
+	collegeid      varchar(6) unique not null,
+  collegename    varchar(40),
+  description    varchar(1000),
+  addressline1   varchar(50),
+  town		  		 varchar(30),
+	postcode       varchar(8),
+  primary key (collegeid)
+) engine=innodb default character set=utf8;
+
+create table location (
+	locationid		varchar(6) unique not null,
+  locationname	varchar(50),
+  locationdes		varchar(1000),
+  collegeid			varchar(6),
+  primary key (locationid),
+  foreign key (collegeid) references college (collegeid)
+) engine=innodb default character set=utf8;
+
+
+create table course (
+	courseid			varchar(8) unique not null,
+  coursename		varchar(30),
+  coursedes			varchar(1000),
+  primary key (courseid)
+) engine=innodb default character set=utf8;
+
+
+create table users (
+	userid				int(8) unique not null,
+  forename			varchar(30),
+  surname				varchar(30),
+  dob						date,
+  telephone			varchar(12),
+  addressline1	varchar(50),
+  town					varchar(30),
+  postcode			varchar(8),
+	email					varchar(40),
+	bio						varchar(1000),
+  usertype			int(1) not null default 2,
+  sessionid			varchar(64),
+  courseid			varchar(8),
+  collegeid 		varchar(6),
+  primary key (userid),
+  foreign key (courseid) references course (courseid),
+  foreign key (collegeid) references college (collegeid)
+) engine=innodb default character set=utf8;
+
+
+
+create table userpass (
+	passwordid	int(10) unique not null,
+  userpass		char(64) not null,
+	salt				char(32) not null,
+  userid			int(8),
+	primary key (passwordid),
+  foreign key (userid) references users (userid)
+) engine=innodb default character set=utf8;
+
+
+
+create table module (
+	moduleid		varchar(8) unique not null,
+  modulename	varchar(100),
+  moduledes		varchar(1000),
+  courseid		varchar(8),
+  primary key (moduleid),
+  foreign key (courseid) references course (courseid)
+) engine=innodb default character set=utf8;
+
+create table mainblog (
+	mainblogid	int(5) not null auto_increment,
+	title				varchar(40),
+	blogtext		text,
+	blogtime		timestamp default current_timestamp,
+	userposter	int(8),
+  collegeid		varchar(6),
+	primary key(mainblogid),
+	foreign key(userposter) references users (userid),
+  foreign key(collegeid) references college (collegeid)
+) engine=innodb default character set=utf8;
+
+create table mainblogcom (
+	mbcid				int(10) not null auto_increment,
+  commenttext	text,
+  blogtime		timestamp default current_timestamp,
+  userid			int(8),
+  mainblogid	int(5),
+  primary key (mbcid),
+  foreign key (userid) references users (userid),
+  foreign key (mainblogid) references mainblog (mainblogid)
+) engine=innodb default character set=utf8;
+
+
+create table courseblog (
+	courseblogid	int(5) not null auto_increment,
+	title					varchar(40),
+	blogtext			text,
+	blogtime			timestamp default current_timestamp,
+	userposter		int(8),
+  courseid			varchar(8),
+  collegeid			varchar(6),
+	primary key (courseblogid),
+	foreign key (userposter) references users (userid),
+  foreign key (courseid) references course (courseid),
+  foreign key (collegeid) references college (collegeid)
+) engine=innodb default character set=utf8;
+
+create table courseblogcom (
+	cbcid					int(10) not null auto_increment,
+  commenttext		text,
+  blogtime			timestamp default current_timestamp,
+  userid				int(8),
+  courseblogid	int(5),
+  primary key (cbcid),
+  foreign key (userid) references users (userid),
+  foreign key (courseblogid) references courseblog (courseblogid)
+) engine=innodb default character set=utf8;
