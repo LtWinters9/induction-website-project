ceb780b46480d016416cb8bb8e152fb92d9bc6d5
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-02-22 12:13:23 +0000

    update v2 commit

--- UHI Induction Service v3/php/logout.php
+++ UHI Induction Service v3/php/logout.php
@@ -113,7 +113,7 @@ session_destroy();
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
-		<script src="assets/js/script.min.js"></script>
+		<script src="../assets/js/script.min.js"></script>
 
 	</body>
 
