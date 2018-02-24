6bae07bdb128ee6caf3977d57741909b8f4df215
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-02-24 02:46:48 +0000

    link update nav

--- Induction/contact/contact-student-services.php
+++ Induction/contact/contact-student-services.php
@@ -59,7 +59,7 @@ $collegeid=$_SESSION['collegeid'];
                 <ul class="nav navbar-nav nav-right">
                     <li class="nav-item" role="presentation"><a class="nav-link" href="https://www.uhi.ac.uk/en/" style="font-family:'Source Sans Pro', sans-serif;color:#ffffff;">Current Students</a></li>
                     <li class="dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="javascript:location.reload()" style="font-family:'Source Sans Pro', sans-serif;color:#ffffff;">My Account</a>
-                        <div class="dropdown-menu" role="menu" style="background-color:#7e3ca6;"><a class="dropdown-item" role="presentation" href="../account/login.php" style="color:#ffffff;background-color:#7e3ca6;">Account Overview </a>
+                        <div class="dropdown-menu" role="menu" style="background-color:#7e3ca6;"><a class="dropdown-item" role="presentation" href="../account/myaccount.php" style="color:#ffffff;background-color:#7e3ca6;">Account Overview </a>
                           <a class="dropdown-item" role="presentation" href="../contact/contact-student-services.php" style="color:#ffffff;background-color:#7e3ca6;">Student Services</a>
 
                         </div>
