f01f6f6b1eb066a0e48bbe9f4d9ae716a62fa782
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-02-24 04:14:57 +0000

    cookie test php

---
+++ Induction/php/cookies.php
@@ -0,0 +1,24 @@
+<!-- Cookies -->
+<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+<script>
+	window.addEventListener("load", function(){
+	window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#7e3ca6",
+      "text": "#ffffff"
+    },
+    "button": {
+      "background": "#9c51c9",
+      "text": "#ffffff"
+    }
+  },
+  "theme": "classic",
+  "content": {
+    "message": "This website uses cookies to ensure you get the best experience on our website, learn more ",
+    "link": "HERE",
+    "href": "https://cookiesandyou.com/"
+  }
+})});
+</script>
