88820111f1620bd40543bc7aecdbe23cd7ed2d74
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-02-24 04:18:20 +0000

    test2

--- Induction/php/cookies.php
+++ Induction/php/cookies.php
@@ -1,14 +1,4 @@
-<?php
-setCookie("userintent","",(time+86400),"/~15011406");
-session_start();
-include('../php/functions.php');
-include('../php/cookies.php');
-$currentuser=getUserLevel();
-$userid=$_SESSION['userid'];
-$forename=$_SESSION['forename'];
-$surname=$_SESSION['surname'];
-$collegeid=$_SESSION['collegeid'];
-?>
+
 
 <!DOCTYPE html>
 <html lang="en">
