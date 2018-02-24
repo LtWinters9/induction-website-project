dd6abe00af4113781ca3bc64a8ac84c2d9d2ce7a
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-02-24 04:11:49 +0000

    added cookies css

--- Induction/blog/collegeblog.php
+++ Induction/blog/collegeblog.php
@@ -44,6 +44,31 @@ $collegeid=$_SESSION['collegeid'];
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="../dist/favicons.ico/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
+
+    <!-- Cookies -->
+    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+    <script>
+    	window.addEventListener("load", function(){
+    	window.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#7e3ca6",
+          "text": "#ffffff"
+        },
+        "button": {
+          "background": "#9c51c9",
+          "text": "#ffffff"
+        }
+      },
+      "theme": "classic",
+      "content": {
+        "message": "This website uses cookies to ensure you get the best experience on our website, learn more ",
+        "link": "HERE",
+        "href": "https://cookiesandyou.com/"
+      }
+    })});
+    </script>
 </head>
 
 <!-- Code is not displaying -- >
