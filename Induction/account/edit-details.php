b32f90d99195791fe60bb1b6ecda659f1a98204c
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-06 14:04:14 +0000

    inc footer

--- Induction/account/edit-details.php
+++ Induction/account/edit-details.php
@@ -133,49 +133,11 @@ $username=checkUser($_SESSION['userid'],session_id(),2);
       </div>
     </form>
   </div>
-  <div class="footer-dark" style="background-color:#7e3ca6;">
-      <footer>
-          <div class="container">
-              <div class="row">
-                  <div class="col-md-6 item text">
-                    <h3 class="text-uppercase" style="color:#ffffff;font-family:'Source Sans Pro', sans-serif;">The University of the Highlands and Islands</h3>
-                    <p><a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/"   target="_blank" style="color:#ffffff;">About</a>
-                      • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/"  target="_blank" style="color:#ffffff;">News and Events</a>
-                      • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/"  target="_blank" style="color:#ffffff;">Business and employers</a>
-                      • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/"  target="_blank" style="color:#ffffff;">Vacancies</a>
-                      • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/"  target="_blank" style="color:#ffffff;">Give to UHI</a>
-                      • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/"  target="_blank" style="color:#ffffff;">FOI</a>
-                      • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/"  target="_blank" style="color:#ffffff;">Cookies</a>
-                      • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/"  target="_blank" style="color:#ffffff;">Contact Us</a></p>
-                  </div>
-                  <div class="col-sm-4 col-md-3 item">
-                      <h3 class="text-uppercase" style="color:#ffffff;font-family:'Source Sans Pro', sans-serif;">About UHI</h3>
-                      <ul>
-                          <li><a href="#" style="color:#ffffff;">Argyll College UHI </a></li>
-                          <li><a href="#">Highland Theological College UHI </a></li>
-                          <li><a href="#">Inverness College UHI </a></li>
-                          <li><a href="https://www.lews.uhi.ac.uk/" target="_blank">Lews Castle College UHI</a> </li>
-                          <li><a href="https://www.moray.uhi.ac.uk/" target="_blank">Moray College UHI</a> </li>
-                          <li><a href="https://www.nafc.uhi.ac.uk/" target="_blank">NAFC Marine Centre UHI</a> </li>
-                          <li><a href="https://www.northhighland.uhi.ac.uk/" target="_blank">North Highland College UHI</a> </li>
-                          <li><a href="https://www.orkney.uhi.ac.uk/" target="_blank">Orkney College UHI</a> </li>
-                      </ul>
-                  </div>
-                  <div class="col-sm-4 col-md-3 item">
-                      <h3 class="text-uppercase" style="color:#ffffff;font-family:'Source Sans Pro', sans-serif;">Information </h3>
-                      <ul>
-                          <li><a href="https://www.uhi.ac.uk/en/" target="_blank">Perth College UHI </a></li>
-                          <li><a href="../info/opening-times.php">Opening Times</a></li>
-                          <li><a href="../info/how-to-find-us/php" style="color:#ffffff;">How To Find Us</a></li>
-                          <li><a href="../info/sitemap.php">Sitemap</a></li>
-                      </ul>
-                  </div>
-                  <div class="col-sm-4 col-md-12 col-xl-12 offset-xl-0 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a></div>
-              </div>
-              <p class="copyright" style="color:#ffffff;">© University of the Highlands and IslandsRegistered office: 12b Ness Walk, Inverness, Scotland, IV3 5SQ&nbsp; </p>
-          </div>
-      </footer>
-  </div>
+  
+  <?php if($currentuser['userlevel']>1) {
+   include "../includes/footer.php";
+    } ?>
+
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
