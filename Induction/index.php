d4162512bc5332d016d546232b95db4c175242c6
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-06 13:44:05 +0000

    syntax error

--- Induction/index.php
+++ Induction/index.php
@@ -1,7 +1,7 @@
 <?php
 setCookie("userintent","",(time+86400),"/~15011406");
 session_start();
-include('../php/functions.php');
+include('php/functions.php');
 $currentuser=getUserLevel();
 $userid=$_SESSION['userid'];
 $forename=$_SESSION['forename'];
