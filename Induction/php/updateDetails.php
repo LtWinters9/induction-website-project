307df7c6233a6c8af122af069c1d31611e02b5f9
Ben <13004417@uhi.ac.uk>
2018-03-08 14:21:19 +0000

    updateDetails
    correction to update query

--- Induction/php/updateDetails.php
+++ Induction/php/updateDetails.php
@@ -9,19 +9,26 @@ date_default_timezone_set('London/GMT');
 require_once("functions.php");
 require_once('db_config.php');
 $currentuser=getUserLevel();
-$staffid=$_SESSION['staffid'];
-$staffname=checkUser($_SESSION['staffid'],session_id(),2);
-if(isset($_POST['userid']) && isset($_POST['username']) && isset($_POST['firstname']) && isset($_POST['surname'])) {
-	if($currentuser['userlevel']<3 && $currentuser['userid'] != $_POST['userid']) {
+$userid=$_SESSION['userid'];
+$username=checkUser($_SESSION['userid'],session_id(),2);
+if(isset($_POST['userid']) && isset($_POST['forename']) && isset($_POST['surname']) && isset($_POST['telephone'])
+ && isset($_POST['mobilephone'])&& isset($_POST['addressline1']) && isset($_POST['addressline2']) && isset($_POST['town']) && isset($_POST['postcode'])) {
+	if($currentuser['userlevel']<2 && $currentuser['userid'] != $_POST['userid']) {
 		header("location: php/logout.php");
 		exit();
 
 	}
 	$db=createConnection();
 	$userid=$_POST['userid'];
-	$username=$_POST['username'];
-	$firstname=$_POST['firstname'];
+	$forename=$_POST['forename'];
 	$surname=$_POST['surname'];
+	$telephone=$_POST['telephone'];
+	$addressline1=$_POST['addressline1'];
+	$addressline2=$_POST['addressline2'];
+	$town=$_POST['town'];
+	$postcode=$_POST['postcode'];
+	$telephone=$_POST['telephone'];
+	$mobilephone=$_POST['mobilephone'];
 	$updatesql="update users set forename=?,surname=?,telphone=?,mobilephone=?,addressline1=?,addressline2=?,town=?,postcode=? where userid=?";
 	$doupdate=$db->prepare($updatesql);
 	$doupdate->bind_param("ssssssssi",$forename,$surname, $telephone, $mobilephone, $addressline1, $addressline2, $town, $postcode,$userid);
