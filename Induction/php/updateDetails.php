a823828e7f8d69a046d5fe800ea17fcb38e29a9d
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-08 23:28:49 +0000

    cmmit change

--- Induction/php/updateDetails.php
+++ Induction/php/updateDetails.php
@@ -1,19 +1,15 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en-gb" dir="ltr">
 <head>Return</head>
 <body>
-
 <?php
 session_start();
-date_default_timezone_set('London/GMT');
-require_once("functions.php");
-require_once('db_config.php');
-$currentuser=getUserLevel();
-$userid=$_SESSION['userid'];
+include("functions.php");
+include("db_config.php");
 $username=checkUser($_SESSION['userid'],session_id(),2);
-if(isset($_POST['userid']) && isset($_POST['forename']) && isset($_POST['surname']) && isset($_POST['addressline1']) && isset($_POST['addressline2'])
-&& isset($_POST['town']) && isset($_POST['postcode'])  && isset($_POST['telephone']) && isset($_POST['mobilephone']) ) {
-	if($currentuser['userlevel']<2 && $currentuser['userid'] != $_POST['userid']) {
+$currentuser=getUserLevel();
+if(isset($_POST['userid']) && isset($_POST['userid']) && isset($_POST['forename']) && isset($_POST['surname'])) {
+	if($currentuser['userlevel']<3 && $currentuser['userid'] != $_POST['userid']) {
 		header("location: php/logout.php");
 		exit();
 
@@ -22,24 +18,17 @@ if(isset($_POST['userid']) && isset($_POST['forename']) && isset($_POST['surname
 	$userid=$_POST['userid'];
 	$forename=$_POST['forename'];
 	$surname=$_POST['surname'];
-	$telephone=$_POST['telephone'];
-	$addressline1=$_POST['addressline1'];
-	$addressline2=$_POST['addressline2'];
-	$town=$_POST['town'];
-	$postcode=$_POST['postcode'];
-	$telephone=$_POST['telephone'];
-	$mobilephone=$_POST['mobilephone'];
-	$updatesql="update users set forename=?,surname=?,addressline1=?,addressline2=?,town=?,postcode=?,telephone=?,mobilephone=? where userid=?";
+	$updatesql="update users set userid=?,forename=?,surname=? where userid=?";
 	$doupdate=$db->prepare($updatesql);
-	$doupdate->bind_param("sssssssss",$forename,$surname,$addressline1,$addressline2,$town,$postcode,$telephone,$mobilephone,$userid);
+	$doupdate->bind_param("sssi",$username,$firstname,$surname,$userid);
 	$doupdate->execute();
 	$doupdate->close();
 	$db->close();
 	if($currentuser['userlevel']==2) {
-			header("location: php/detailsUpdated.php ");
+			header("location: ../account/myaccount.php");
 			exit();
 	} else {
-		header("location: php/logout.php ");
+		header("location: php/logout.php");
 			exit();
 
 	}
@@ -48,6 +37,5 @@ if(isset($_POST['userid']) && isset($_POST['forename']) && isset($_POST['surname
 	echo "<p>Some parameters are missing, cannot update database</p>";
 }
 ?>
-
 </body>
 </html>
