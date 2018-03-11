a4f9cbb08381e8211912ea1296ab884316b9846d
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-11 05:00:33 +0000

    can not INCLUDE

--- Induction/account/myaccount.php
+++ Induction/account/myaccount.php
@@ -86,14 +86,42 @@ $userid=checkUser($_SESSION['userid'],session_id(),2);
                   ?>. Welcome to your account, <?php echo $forename; ?></h2>
                   <p class="lead text-center">Access the key areas quickly below.</p>
               </div>
+          </div>
+    </div>
 
-
-              <?php if($currentuser['userlevel']>1) {
-                include "../includes/main-account.php";
-              }  ?>
-
+    <div class="row justify-content-center features">
+        <div class="col-sm-6 col-md-5 col-lg-4 item" data-aos="zoom-in-down" data-aos-duration="200" data-aos-delay="200" data-aos-once="true">
+            <div class="box"><i class="icon ion-chatbubbles icon" style="color:#7e3ca6;"></i>
+                <h3 class="name">Chatbot </h3>
+                <p class="description">Chat with our bot now for any student releated questions.&nbsp;.</p>This may include payment dates and campus details<a href="#" class="learn-more">Ask a question »</a></div>
+        </div>
+        <div class="col-sm-6 col-md-5 col-lg-4 item">
+            <div data-aos="zoom-in-down" data-aos-duration="300" data-aos-delay="300" data-aos-once="true" class="box"><i class="fa fa-file-zip-o icon" style="color:#7e3ca6;"></i>
+                <h3 class="name">Fourms and Guidance </h3>
+                <p class="description">Browse though the files that are related to your current course.&nbsp;.</p>This will ensure you're ready for your first day.<a href="#" class="learn-more">Browse now »</a></div>
+        </div>
+        <div class="col-sm-6 col-md-5 col-lg-4 item">
+            <div data-aos="zoom-in-down" data-aos-duration="400" data-aos-delay="400" data-aos-once="true" class="box"><i class="icon ion-images icon" style="color:#7e3ca6;"></i>
+                <h3 class="name">Tour the Campus </h3>
+                <p class="description">Want to get a birds eye view of Perth UHI?</p>Click below to view our 360° of Perth UHI<a href="#" class="learn-more">View now »</a></div>
+        </div>
+        <div class="col-sm-6 col-md-5 col-lg-4 item">
+            <div data-aos="zoom-in-down" data-aos-duration="500" data-aos-delay="500" data-aos-once="true" class="box"><i class="icon ion-android-chat icon" style="color:#7e3ca6;"></i>
+                <h3 class="name">Fourms </h3>
+                <p class="description">Want to break the ice with your fellow students?&nbsp;.</p>Click below to get chatting.<a href="../blog/collegeblog.php" class="learn-more">Chat now »</a></div>
+        </div>
+        <div class="col-sm-6 col-md-5 col-lg-4 item">
+            <div data-aos="zoom-in-down" data-aos-duration="600" data-aos-delay="600" data-aos-once="true" class="box"><i class="material-icons icon" style="color:#7e3ca6;">contact_mail</i>
+                <h3 class="name">Contact Student Services</h3>
+                <p class="description">Need to get in touch with student services?</p>Click the link below to send an email.<a href="#" class="learn-more">Contact now »</a></div>
+        </div>
+        <div class="col-sm-6 col-md-5 col-lg-4 item">
+            <div data-aos="zoom-in-down" data-aos-duration="700" data-aos-delay="700" data-aos-once="true" class="box"><i class="fa fa-users icon" style="color:#7e3ca6;"></i>
+                <h3 class="name">Meet your Lectuers </h3>
+                <p class="description">Want to know who your Lectuers will be for your course?&nbsp; </p>Click below to learn more about them, or get their contact details<a href="#" class="learn-more">Learn more »</a></div>
         </div>
     </div>
+
     <div class="highlight-clean" style="background-color:#dfe8ee;">
         <div class="container" data-aos="slide-right" data-aos-duration="800" data-aos-delay="500" data-aos-once="true">
             <div class="intro">
