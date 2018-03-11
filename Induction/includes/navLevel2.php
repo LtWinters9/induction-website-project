50773f8da7ad12da138f7c51a01b4e2ba1907a13
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-11 06:09:54 +0000

    update links

--- Induction/includes/navLevel2.php
+++ Induction/includes/navLevel2.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-light navbar-expand-md navbar-fixed-top navigation-clean-button" style="background-color:#7e3ca6;">
-    <div class="container"><a class="navbar-brand" href="#"> <span style="color:#9da9ae;font-size:20px;"><img class="img-fluid" src="../assets/img/UHI_Logo_RGB_transparent_orig.png" alt="uhi-main-logo" style="color:#ffffff;width:140px;"> </span></a><button class="navbar-toggler" data-toggle="collapse"
+    <div class="container"><a class="navbar-brand" href="../web/index.php"> <span style="color:#9da9ae;font-size:20px;"><img class="img-fluid" src="../assets/img/UHI_Logo_RGB_transparent_orig.png" alt="uhi-main-logo" style="color:#ffffff;width:140px;"> </span></a><button class="navbar-toggler" data-toggle="collapse"
             data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navcol-1">
             <ul class="nav navbar-nav nav-right">
@@ -12,7 +12,7 @@
                         <a class="dropdown-item" role="presentation" href="../php/logout.php" style="color:#ffffff;background-color:#7e3ca6;">Logout</a>
                     </div>
                 </li>
-                
+
                 <li class="nav-item" role="presentation"><a class="nav-link" href="#" style="font-family:'Roboto Condensed', sans-serif;color:#ffffff;"><i class="fa fa-globe"></i>&nbsp;Alumni </a></li>
                 <li class="nav-item" role="presentation"><a class="nav-link" href="#" data-bs-hover-animate="pulse" style="font-family:'Roboto Condensed', sans-serif;color:#ffffff;"><i class="fa fa-globe"></i>&nbsp;Gàidhlig </a></li>
             </ul>
