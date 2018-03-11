ca098a1642abef4a921e186cb9f1e44589e30a42
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-11 09:18:28 +0000

    fix later
    bootstrap has an issuie with $row

--- Induction/contact/booking.php
+++ Induction/contact/booking.php
@@ -9,30 +9,29 @@ $userid=$_SESSION['userid'];
 $forename=$_SESSION['forename'];
 $surname=$_SESSION['surname'];
 $collegeid=$_SESSION['collegeid'];
-$userid=checkUser($_SESSION['userid'],session_id(),2);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
+<?php if($currentuser['userlevel']<1) {
+    include "../includes/banner.html";
+} ?>
+
 <?php if($currentuser['userlevel']>1) {
-   include "../includes/banner.html";
-    } ?>
+    include "../includes/banner.html";
+} ?>
 
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Welcome to your account,
-    <?php echo $forename; ?> | UHI Induction Services</title>
-
-  <!-- bootstrap css libary -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Contact | UHI Induction Services</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-icons/3.0.1/iconfont/material-icons.min.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,700">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Condensed:400,700">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:300,400">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
@@ -65,129 +64,128 @@ $userid=checkUser($_SESSION['userid'],session_id(),2);
 </head>
 
 <body>
-
+  <!-- Nav Bar -->
 
   <?php if($currentuser['userlevel']>1) {
     include "../includes/navLevel2.php";
-     } ?>
+} ?>
 
-     <!-- Book Section -->
-     <body>
-       <div><div class="jumbotron-contact jumbotron-contact-sm">
-       <div class="container">
-           <div class="row">
-               <div class="col-sm-12 col-lg-12">
-                   <h1 class="h1">
-                       Contact us <small>Feel free to contact us</small></h1>
-               </div>
-           </div>
-       </div>
-   </div>
-   <div class="container">
-       <div class="row">
-           <div class="col-md-8">
-               <div class="well well-sm">
-                   <form>
-                   <div class="row">
-                       <div class="col-md-6">
-                           <div class="form-group">
-                               <label for="subject">
-                                   Subject</label>
-                               <select id="subject" name="subject" class="form-control" required="required">
-                                   <option value="na" selected="">Choose One:</option>
-                                   <option value="service">General Customer Service</option>
-                                   <option value="suggestions">Suggestions</option>
-                                   <option value="product">Product Support</option>
-                               </select>
-                           </div>
-                       </div>
-                       </form>
+  <!-- Book Section -->
+
+  <body>
+    <form action="confirmation.php" method="post">
+      <div class="row">
+        <div class="col-md-6">
+            <div class="form-group">
+      <?php
+$conn = createConnection();
+$result = $conn->query("select bookingid, bookdate, booktime, advisor, booked from booking where booked = 'N'");
+
+while($row = mysqli_fetch_array($result))
+
+{
 
 
 
-                   </div>
+                            <label for="name">
+                                Name</label>
+                            <input type="text" class="form-control" id="name" placeholder="Enter name" required="required" />
+                        </div>
+                        <div class="form-group">
+                            <label for="email">
+                                Email Address</label>
+                            <div class="input-group">
+                                <span class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+                                </span>
+                                <input type="email" class="form-control" id="email" placeholder="Enter email" required="required" /></div>
+                        </div>
+                        <div class="form-group">
+                            <label for="subject">
+                                Subject</label>
+                            <select id="subject" name="subject" class="form-control" required="required">
+                                <option value="na" selected="">Choose One:</option>
+                                <option value="service">General Customer Service</option>
+                                <option value="suggestions">Suggestions</option>
+                                <option value="product">Product Support</option>
+                            </select>
+                        </div>
+                    </div>
 
-               </div>
-           </div>
-          </div>
-   </div></div>
-     <?php
+                    </div>
 
+                }
 
-     $conn = createConnection();
-     $result = $conn->query("select bookingid, bookdate, booktime, advisor, booked from booking where booked = 'N'");
+                echo "</select>";
 
-     echo '<b>Please select an appointment from the drop down below:</b>';
-     echo '<b>Available Appointments:</b>';
-     echo '</br></br>';
-     echo "<select name = 'optionselected'>";
-     while($row = mysqli_fetch_array($result)) {
-         echo "<option value = ",$row[bookingid],">", " Date: ", $row[bookdate], " Time: ", $row[booktime]," Advisor: ", $row[advisor],"</option>";
-     }
-     echo "</select>";
+                $conn->close();
+                ?>
+    </form>
 
-     $conn->close();
-     ?>
+    <div class="col-md-12">
+        <button type="submit" class="btn btn-primary pull-right" id="book">Book Now</button>
+    </div>
+  </div>
+
+  </body>
 
 
 
-     <div class="col-md-12">
-         <button type="submit" class="btn btn-primary pull-right" id="btnContactUs">
-             Send Message</button>
-     </div>
-     </form>
-
-
-
-    <div data-aos="fade-right" data-aos-once="true" class="highlight-clean">
-      <div class="container">
-        <div class="intro">
-          <h2 class="text-center" data-aos="fade-left" data-aos-duration="800" data-aos-delay="100" data-aos-once="true" style="font-family:'Source Sans Pro', sans-serif;">Questions about your student funding?</h2>
-        </div>
-        <div class="buttons"><a class="btn btn-light" role="button" href="account/login.php" data-aos="fade-up" data-aos-duration="900" data-aos-delay="200" data-aos-once="true" style="font-family:'Source Sans Pro', sans-serif;background-color:#7e3ca6;color:#ffffff;">CHAT NOW</a></div>
+  <div class="testimonials-clean"></div>
+  <div data-aos="fade-right" data-aos-once="true" class="highlight-clean">
+    <div class="container">
+      <div class="intro">
+        <h2 class="text-center" data-aos="fade-left" data-aos-duration="800" data-aos-delay="100" data-aos-once="true" style="font-family:'Roboto Condensed', sans-serif;">Questions about your student experience?</h2>
       </div>
+      <div class="buttons"><a class="btn btn-light" role="button" href="../account/login.php" data-aos="fade-up" data-aos-duration="900" data-aos-delay="200" data-aos-once="true" style="font-family:'Roboto Condensed', sans-serif;background-color:#7e3ca6;color:#ffffff;">CHAT NOW</a></div>
     </div>
-    <div class="footer-dark" style="background-color:#7e3ca6;">
-      <footer>
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6 item text">
-              <h3 class="text-uppercase" style="color:#ffffff;font-family:'Source Sans Pro', sans-serif;">The University of the Highlands and Islands</h3>
-              <p>About • News and events • Business and employers • Vacancies • Give to UHI • FOI • Cookies • Contact Us</p>
-            </div>
-            <div class="col-sm-4 col-md-3 item">
-              <h3 class="text-uppercase" style="color:#ffffff;font-family:'Source Sans Pro', sans-serif;">About UHI</h3>
-              <ul>
-                <li><a href="#" style="color:#ffffff;">Argyll College UHI </a></li>
-                <li><a href="#">Highland Theological College UHI </a></li>
-                <li><a href="#">Inverness College UHI </a></li>
-                <li><a href="https://www.lews.uhi.ac.uk/" target="_blank">Lews Castle College UHI</a> </li>
-                <li><a href="https://www.moray.uhi.ac.uk/" target="_blank">Moray College UHI</a> </li>
-                <li><a href="https://www.nafc.uhi.ac.uk/" target="_blank">NAFC Marine Centre UHI</a> </li>
-                <li><a href="https://www.northhighland.uhi.ac.uk/" target="_blank">North Highland College UHI</a> </li>
-                <li><a href="https://www.orkney.uhi.ac.uk/" target="_blank">Orkney College UHI</a> </li>
-              </ul>
-            </div>
-            <div class="col-sm-4 col-md-3 item">
-              <h3 class="text-uppercase" style="color:#ffffff;font-family:'Source Sans Pro', sans-serif;">Information </h3>
-              <ul>
-                <li><a href="https://www.uhi.ac.uk/en/">Perth College UHI </a></li>
-                <li><a href="#">Opening Times</a></li>
-                <li><a href="#" style="color:#ffffff;">How To Find Us</a></li>
-                <li>Site map</li>
-              </ul>
-            </div>
-            <div class="col-sm-4 col-md-12 col-xl-12 offset-xl-0 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a></div>
+  </div>
+  <div class="testimonials-clean"></div>
+  <div class="footer-dark" style="background-color:#7e3ca6;">
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 item text">
+            <h3 class="text-uppercase" style="color:#ffffff;font-family:'Roboto Condensed', sans-serif;">The University of the Highlands and Islands</h3>
+            <p><a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">About</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">News and Events</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/"
+                target="_blank" style="color:#ffffff;">Business and employers</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">Vacancies</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/"
+                target="_blank" style="color:#ffffff;">Give to UHI</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">FOI</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank"
+                style="color:#ffffff;">Cookies</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">Contact Us</a></p>
           </div>
-          <p class="copyright" style="color:#ffffff;">© University of the Highlands and IslandsRegistered office: 12b Ness Walk, Inverness, Scotland, IV3 5SQ&nbsp; </p>
+          <div class="col-sm-4 col-md-3 item">
+            <h3 class="text-uppercase" style="color:#ffffff;font-family:'Roboto Condensed', sans-serif;">About UHI</h3>
+            <ul>
+              <li><a href="#" style="color:#ffffff;">Argyll College UHI </a></li>
+              <li><a href="#">Highland Theological College UHI </a></li>
+              <li><a href="#">Inverness College UHI </a></li>
+              <li><a href="https://www.lews.uhi.ac.uk/" target="_blank">Lews Castle College UHI</a> </li>
+              <li><a href="https://www.moray.uhi.ac.uk/" target="_blank">Moray College UHI</a> </li>
+              <li><a href="https://www.nafc.uhi.ac.uk/" target="_blank">NAFC Marine Centre UHI</a> </li>
+              <li><a href="https://www.northhighland.uhi.ac.uk/" target="_blank">North Highland College UHI</a> </li>
+              <li><a href="https://www.orkney.uhi.ac.uk/" target="_blank">Orkney College UHI</a> </li>
+            </ul>
+          </div>
+          <div class="col-sm-4 col-md-3 item">
+            <h3 class="text-uppercase" style="color:#ffffff;font-family:'Roboto Condensed', sans-serif;">Information </h3>
+            <ul>
+              <li><a href="https://www.uhi.ac.uk/en/" target="_blank">Perth College UHI </a></li>
+              <li><a href="../info/opening-times.php">Opening Times</a></li>
+              <li><a href="../info/how-to-find-us/php" style="color:#ffffff;">How To Find Us</a></li>
+              <li><a href="../info/sitemap.php">Sitemap</a></li>
+            </ul>
+          </div>
+          <div class="col-sm-4 col-md-12 col-xl-12 offset-xl-0 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a></div>
         </div>
-      </footer>
-    </div>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
-    <script src="assets/js/script.min.js"></script>
-
+        <p class="copyright" style="color:#ffffff;">© University of the Highlands and IslandsRegistered office: 12b Ness Walk, Inverness, Scotland, IV3 5SQ&nbsp; </p>
+      </div>
+    </footer>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
+  <script src="../dist/js/contact_me.js"></script>
+  <script src="../dist/js/jqBootstrapValidation.js"></script>
+  <script src="../assets/js/script.min.js"></script>
+  <script src="../dist/js/cookies.js"></script>
 
 </body>
 
