7058db20103bc74622cbde689372ac9e30a93658
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-13 13:02:50 +0000

    update ljnks

--- Induction/account/myaccount.php
+++ Induction/account/myaccount.php
@@ -90,18 +90,18 @@ $userid=checkUser($_SESSION['userid'],session_id(),2);
                   <div class="col-sm-6 col-md-5 col-lg-4 item" data-aos="zoom-in-down" data-aos-duration="200" data-aos-delay="200" data-aos-once="true">
                       <div class="box"><i class="icon ion-chatbubbles icon" style="color:#7e3ca6;"></i>
                           <h3 class="name" style="font-family:'Roboto Condensed', sans-serif;">Chatbot </h3>
-                          <p class="text-center description" style="font-family:'Roboto Condensed', sans-serif;font-size:16px;">Chat with our bot now for any student releated questions. &nbsp;This may include payment dates, libary location,<br><strong>campus details and&nbsp;</strong>where to get that much needed coffee.</p><a href="#" class="learn-more"
+                          <p class="text-center description" style="font-family:'Roboto Condensed', sans-serif;font-size:16px;">Chat with our bot now for any student releated questions. &nbsp;This may include payment dates, libary location,<br><strong>campus details and&nbsp;</strong>where to get that much needed coffee.</p><a href="https://landbot.io/u/H-26103-U8N9IIM4FGVTSVVP/index.html" class="learn-more"
                               style="font-family:'Roboto Condensed', sans-serif;">Ask a question»</a></div>
                   </div>
                   <div class="col-sm-6 col-md-5 col-lg-4 item">
                       <div data-aos="zoom-in-down" data-aos-duration="300" data-aos-delay="300" data-aos-once="true" class="box"><i class="fa fa-file-zip-o icon" style="color:#7e3ca6;"></i>
                           <h3 class="name" style="font-family:'Roboto Condensed', sans-serif;">Fourms and Guidance </h3>
-                          <p class="text-center description" style="font-family:'Roboto Condensed', sans-serif;font-size:16px;">Browse though the files that are related to your current course. &nbsp;This will ensure you're ready for your first day, or stay ontop of your current course work with updates.</p><a href="#" class="learn-more" style="font-family:'Roboto Condensed', sans-serif;">Browse now »</a></div>
+                          <p class="text-center description" style="font-family:'Roboto Condensed', sans-serif;font-size:16px;">Browse though the files that are related to your current course. &nbsp;This will ensure you're ready for your first day, or stay ontop of your current course work with updates.</p><a href="../course/hnd-computing.php" class="learn-more" style="font-family:'Roboto Condensed', sans-serif;">Browse now »</a></div>
                   </div>
                   <div class="col-sm-6 col-md-5 col-lg-4 item">
                       <div data-aos="zoom-in-down" data-aos-duration="400" data-aos-delay="400" data-aos-once="true" class="box"><i class="icon ion-images icon" style="color:#7e3ca6;"></i>
                           <h3 class="name" style="font-family:'Roboto Condensed', sans-serif;">Tour the Campus </h3>
-                          <p class="text-center description" style="font-family:'Roboto Condensed', sans-serif;font-size:16px;"><strong>Want to get a birds eye view of Perth UHI, or get to know your surroundings before you start? Click the link below to view our 360° of Perth UHI.&nbsp;</strong><br><br></p><a href="#" class="learn-more" style="font-family:'Roboto Condensed', sans-serif;">View now »</a></div>
+                          <p class="text-center description" style="font-family:'Roboto Condensed', sans-serif;font-size:16px;"><strong>Want to get a birds eye view of Perth UHI, or get to know your surroundings before you start? Click the link below to view our 360° of Perth UHI.&nbsp;</strong><br><br></p><a href="../tour-the-campus.php" class="learn-more" style="font-family:'Roboto Condensed', sans-serif;">View now »</a></div>
                   </div>
                   <div class="col-sm-6 col-md-5 col-lg-4 item">
                       <div data-aos="zoom-in-down" data-aos-duration="500" data-aos-delay="500" data-aos-once="true" class="box"><i class="icon ion-android-chat icon" style="color:#7e3ca6;"></i>
@@ -111,12 +111,12 @@ $userid=checkUser($_SESSION['userid'],session_id(),2);
                   <div class="col-sm-6 col-md-5 col-lg-4 item">
                       <div data-aos="zoom-in-down" data-aos-duration="600" data-aos-delay="600" data-aos-once="true" class="box"><i class="material-icons icon" style="color:#7e3ca6;">contact_mail</i>
                           <h3 class="name" style="font-family:'Roboto Condensed', sans-serif;">Contact us now </h3>
-                          <p class="description" style="font-family:'Roboto Condensed', sans-serif;font-size:16px;">Want to break the ice with your fellow students, or just generally want to get to know everyone before your first day? Great! click below now to get started.&nbsp;</p><a href="#" class="learn-more" style="font-family:'Roboto Condensed', sans-serif;">Contact now »</a></div>
+                          <p class="description" style="font-family:'Roboto Condensed', sans-serif;font-size:16px;">Want to break the ice with your fellow students, or just generally want to get to know everyone before your first day? Great! click below now to get started.&nbsp;</p><a href="../blog/collegeblog.php" class="learn-more" style="font-family:'Roboto Condensed', sans-serif;">Contact now »</a></div>
                   </div>
                   <div class="col-sm-6 col-md-5 col-lg-4 item">
                       <div data-aos="zoom-in-down" data-aos-duration="700" data-aos-delay="700" data-aos-once="true" class="box"><i class="fa fa-users icon" style="color:#7e3ca6;"></i>
                           <h3 class="name" style="font-family:'Roboto Condensed', sans-serif;">Meet your Lectuers </h3>
-                          <p class="description" style="font-family:'Roboto Condensed', sans-serif;font-size:16px;">Need to know how to get in touch with one of your lectuers, or simply want to know who they're before you start? Click now below to find out more information.</p><a href="#" class="learn-more" style="font-family:'Roboto Condensed', sans-serif;">Learn more »</a></div>
+                          <p class="description" style="font-family:'Roboto Condensed', sans-serif;font-size:16px;">Need to know how to get in touch with one of your lectuers, or simply want to know who they're before you start? Click now below to find out more information.</p><a href="../course/hnd-computing.php" class="learn-more" style="font-family:'Roboto Condensed', sans-serif;">Learn more »</a></div>
                   </div>
               </div>
           </div>
