81204a4f3424c4b69698830269eea98d053d5ff4
David Ferguson <Fergie_32@hotmail.com>
2018-03-15 09:46:44 +0000

    database update

--- Induction/php/db_config.php
+++ Induction/php/db_config.php
@@ -1,10 +1,10 @@
 <?php
 
 function createConnection() {
-	$host="localhost";
-	$user="induction_Conn";
-	$userpass='ivtB35?CE7wcpwxm';
-	$schema="inductionDB";
+	$host="comp-server.uhi.ac.uk";
+	$user="pe15011406";
+	$userpass='triple30';
+	$schema="pe15011406";
 	$conn = new mysqli($host,$user,$userpass,$schema);
 	if(mysqli_connect_errno()) {
 		echo "Sorry, could not connect to server. Please try again later.: ".mysqli_connect_errno();
