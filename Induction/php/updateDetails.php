491687b3a80b86d5bd52a3a34cd0514e722afece
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-15 12:04:00 +0000

    variables

--- Induction/php/updateDetails.php
+++ Induction/php/updateDetails.php
@@ -9,6 +9,18 @@ include("db_config.php");
 $userid=$_SESSION['userid'];
 
 if(isset($userid)) {
+
+	if(isset($userid)) {
+	$db=createConnection();
+	$forename=$_POST['forename'];
+	$surname=$_POST['surname'];
+	$addressline1=$_POST['addressline1'];
+	$addressline2=$_POST['addressline1'];
+	$town=$_POST['town'];
+	$postcode=$_POST['postcpde'];
+	$telephone=$_POST['telephone'];
+	$mobilephone=$_POST['mobilephone'];
+
 	$updatesql="update users set forename=?,surname=?,addressline1=?,addressline2=?,town=?,postcode=?,telephone=?, mobilephone=? where userid=?";
     $doupdate=$db->prepare($updatesql);
     $doupdate->bind_param("ssssssssi", $forename,$surname,$addressline1,$addressline2,$town,$postcode,$telephone, $mobilephone, $userid);
