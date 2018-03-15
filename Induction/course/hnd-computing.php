e2112913ef97a0776811002fbf9e0301bb13530b
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-15 11:22:02 +0000

    add includes slide

--- Induction/course/hnd-computing.php
+++ Induction/course/hnd-computing.php
@@ -151,16 +151,9 @@ $modulename=$_POST['modulename'];
       </div>
     </div>
 
-    <div class="testimonials-clean"></div>
-    <div data-aos="fade-right" data-aos-once="true" class="highlight-clean">
-      <div class="container">
-        <div class="intro">
-          <h2 class="text-center" data-aos="fade-left" data-aos-duration="800" data-aos-delay="100" data-aos-once="true" style="font-family:'Roboto Condensed', sans-serif;">Questions about your student funding?</h2>
-        </div>
-        <div class="buttons"><a class="btn btn-light" role="button" href="account/login.php" data-aos="fade-up" data-aos-duration="900" data-aos-delay="200" data-aos-once="true" style="font-family:'Roboto Condensed', sans-serif;background-color:#7e3ca6;color:#ffffff;">CHAT NOW</a></div>
-      </div>
-    </div>
-    <div class="testimonials-clean"></div>
+    <?php if($currentuser['userlevel']>1) {
+     include "../includes/slide-in.php";
+      } ?>
 
     <?php if($currentuser['userlevel']>1) {
      include "../includes/footer.php";
---
+++ Induction/includes/slide-in.php
@@ -0,0 +1,10 @@
+<div class="testimonials-clean"></div>
+<div data-aos="fade-right" data-aos-once="true" class="highlight-clean">
+  <div class="container">
+    <div class="intro">
+      <h2 class="text-center" data-aos="fade-left" data-aos-duration="800" data-aos-delay="100" data-aos-once="true" style="font-family:'Roboto Condensed', sans-serif;">Questions about HND Computing <?php echo $modulename; ?> ? </h2>
+    </div>
+    <div class="buttons"><a class="btn btn-light" role="button" href="account/login.php" data-aos="fade-up" data-aos-duration="900" data-aos-delay="200" data-aos-once="true" style="font-family:'Roboto Condensed', sans-serif;background-color:#7e3ca6;color:#ffffff;">CHAT NOW</a></div>
+  </div>
+</div>
+<div class="testimonials-clean"></div>
