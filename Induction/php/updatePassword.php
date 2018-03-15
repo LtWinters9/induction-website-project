ed76d6239c394ed300a676a9aaf027bffdb2d46e
Ben <13004417@uhi.ac.uk>
2018-03-15 12:16:00 +0000

    .

--- Induction/php/updatePassword.php
+++ Induction/php/updatePassword.php
@@ -15,42 +15,15 @@ $userpass=$_POST['inputPassword'];
 		exit();
 	}
 echo "connection created";
-
-//Create query, note that parameters being passed in are represented by question marks
-$loginsql="select userpass, salt, hash from userpass where userpass.userid=?";
-$lgnstmt = $db->prepare($loginsql);
-//Bound parameters are defined by type, s = string, i = integer, d = double and b = blob
-$lgnstmt->bind_param("isss",$userid,$userpass,$hash,$salt);
-//Run query
-$lgnstmt->execute();
-//Store Query Result
-$lgnstmt->store_result();
-
-//Bind returned row parameters in same order as they appear in query
-$lgnstmt->bind_result($userpass,$salt,$hash);
-
-//Valid login only if exactly one row returned, otherwise something iffy is going on
-if($lgnstmt->num_rows()==1) {
-    //Fetch the next (it should be only) row from the returned results
-    $lgnstmt->fetch();
-    $cyphertext=makeHash($userpass,$salt,50);
-    $lgnstmt->close();
-    if($cyphertext==$hash) {
-        //Update user's record with session data
-        $sessionsql="update userpass set sessionid=? where userid=?";
-        $sessionstmt=$db->prepare($sessionsql);
-        $sessionstmt->bind_param("si",session_id(),$studentid);
-        $sessionstmt->execute();
-        $sessionstmt->close();
-
-}
-
-$updatesql= 'UPDATE `userpass` SET `userpass` = \'?\', `hash` = \'?\', `salt` = \'?\' WHERE `userpass`.`userid` = \'?\'';
-$updateP=$db->prepare($updatesql);
-$updateP->bind_param("isss",$userid,$userpass,$hash,$salt);
-$updateP->execute();
-$updateP->close();
-
+$loginsql="select userpass, salt from userpass where userid=?";
+$cyphertext=makeHash($userpass,$salt,50);
+if(isset($userid)) {
+	$updatesql="update userpass set userpass=?, salt=?, hash=? where userid=?";
+    $doupdate=$db->prepare($updatesql);
+    $doupdate->bind_param("sssi", $userpass,$salt,$hash,$userid);
+    $doupdate->execute();
+    $doupdate->close();
+    $db->close();
 	if($currentuser['userlevel']>=2) {
 			header("location: ../account/myaccount.php");
 			exit();
@@ -63,5 +36,5 @@ $updateP->close();
 } else {
 	echo "<p>Can not update details, Please return.</p>";
 }
-  $db->close();
+
  ?>
