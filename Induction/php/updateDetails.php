72ead8413866af9a3d817ebde7f7528130a81159
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-15 11:55:54 +0000

    roll back

--- Induction/php/updateDetails.php
+++ Induction/php/updateDetails.php
@@ -1,51 +1,30 @@
-<!doctype html>
 <html lang="en-gb" dir="ltr">
 <head>Return</head>
 <body>
+
 <?php
 session_start();
 include("functions.php");
 include("db_config.php");
+$userid=$_SESSION['userid'];
 
-$userid=checkUser($_SESSION['userid'],session_id(),2);
+if(isset($userid)) {
+	$updatesql="update users set forename=?,surname=?,addressline1=?,addressline2=?,town=?,postcode=?,telephone=?, mobilephone=? where userid=?";
+    $doupdate=$db->prepare($updatesql);
+    $doupdate->bind_param("ssssssssi", $forename,$surname,$addressline1,$addressline2,$town,$postcode,$telephone, $mobilephone, $userid);
+    $doupdate->execute();
+    $doupdate->close();
 
-if(isset($_POST['userid']) && isset($_POST['userid']) && isset($_POST['forename']) && isset($_POST['surname'])) {
-	if($currentuser['userlevel']<2 && $currentuser['userid'] != $_POST['userid']) {
-		header("location: php/logout.php");
-		exit();
 
-	}
-	$db=createConnection();
-	$userid=$_POST['userid'];
-	$forename=$_POST['forename'];
-	$surname=$_POST['surname'];
-	$addressline1=$_POST['addressline1'];
-	$addressline2=$_POST['addressline2'];
-	$town=$_POST['town'];
-	$postcode=$_POST['postcode'];
-	$telephone=$_POST['telephone'];
-	$mobilephone=$_POST['mobilephone'];
-
-echo "connection created";
-/*
-	$updatesql= 'UPDATE `users` SET `forename` = \'?\', `surname` = \'?\', `addressline1` = \'?\', `addressline2` = \'?\', `town` = \'?\', `postcode` = \'?\', `telephone` = \'?\', `mobilephone` = \'?\' WHERE `users`.`userid` = \'?\'';
-	$doupdate=$db->prepare($updatesql);
-	$doupdate->bind_param("issssssss",$userid,$forename,$surname,$addressline1,$addressline2,$town,$postcode,$telephone,$mobilephone);
-	$doupdate->execute();
-	$doupdate->close();
-	$db->close();
-	*/
-	if($currentuser['userlevel']>1) {
-			header("location: ../account/myaccount.php");
-			exit();
-	} else {
-		header("location: php/logout.php");
-			exit();
-
-	}
+		echo "<script>
+alert('The changes have been made to your account');
+window.location.href='../account/myaccount.php';
+</script>";
 
 } else {
-	echo "<p>Can not update details, Please return.</p>";
+
+echo "<p>Some parameters are missing, cannot update database</p>";
+
 }
 ?>
 </body>
