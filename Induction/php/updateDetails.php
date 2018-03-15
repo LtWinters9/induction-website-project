6291dd49f4bd4abb9195671cb2f0805540b04d3b
Ben <13004417@uhi.ac.uk>
2018-03-15 09:37:24 +0000

    update details test

--- Induction/php/updateDetails.php
+++ Induction/php/updateDetails.php
@@ -26,13 +26,14 @@ if(isset($_POST['userid']) && isset($_POST['userid']) && isset($_POST['forename'
 	$mobilephone=$_POST['mobilephone'];
 
 echo "connection created";
-
+/*
 	$updatesql= 'UPDATE `users` SET `forename` = \'?\', `surname` = \'?\', `addressline1` = \'?\', `addressline2` = \'?\', `town` = \'?\', `postcode` = \'?\', `telephone` = \'?\', `mobilephone` = \'?\' WHERE `users`.`userid` = \'?\'';
 	$doupdate=$db->prepare($updatesql);
 	$doupdate->bind_param("issssssss",$userid,$forename,$surname,$addressline1,$addressline2,$town,$postcode,$telephone,$mobilephone);
 	$doupdate->execute();
 	$doupdate->close();
 	$db->close();
+	*/
 	if($currentuser['userlevel']==2) {
 			header("location: ../account/myaccount.php");
 			exit();
