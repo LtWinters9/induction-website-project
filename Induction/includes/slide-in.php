034974faf4671713af8a35b38165e711f222e654
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-15 11:23:06 +0000

    commit change

--- Induction/includes/slide-in.php
+++ Induction/includes/slide-in.php
@@ -4,7 +4,7 @@
     <div class="intro">
       <h2 class="text-center" data-aos="fade-left" data-aos-duration="800" data-aos-delay="100" data-aos-once="true" style="font-family:'Roboto Condensed', sans-serif;">Questions about HND Computing <?php echo $modulename; ?> ? </h2>
     </div>
-    <div class="buttons"><a class="btn btn-light" role="button" href="account/login.php" data-aos="fade-up" data-aos-duration="900" data-aos-delay="200" data-aos-once="true" style="font-family:'Roboto Condensed', sans-serif;background-color:#7e3ca6;color:#ffffff;">CHAT NOW</a></div>
+    <div class="buttons"><a class="btn btn-light" role="button" href="https://landbot.io/u/H-26103-U8N9IIM4FGVTSVVP/index.html" target="_blank" data-aos="fade-up" data-aos-duration="900" data-aos-delay="200" data-aos-once="true" style="font-family:'Roboto Condensed', sans-serif;background-color:#7e3ca6;color:#ffffff;">CHAT NOW</a></div>
   </div>
 </div>
 <div class="testimonials-clean"></div>
