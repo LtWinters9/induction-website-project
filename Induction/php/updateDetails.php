c93e01c3087fdf4229d0d45f0c3355e3f2af01d6
Ben <13004417@uhi.ac.uk>
2018-03-15 09:29:39 +0000

    .

--- Induction/php/updateDetails.php
+++ Induction/php/updateDetails.php
@@ -29,7 +29,7 @@ echo "connection created";
 
 	$updatesql= 'UPDATE `users` SET `forename` = \'?\', `surname` = \'?\', `addressline1` = \'?\', `addressline2` = \'?\', `town` = \'?\', `postcode` = \'?\', `telephone` = \'?\', `mobilephone` = \'?\' WHERE `users`.`userid` = \'?\'';
 	$doupdate=$db->prepare($updatesql);
-	$doupdate->bind_param("isssssssi",$userid,$forename,$surname,$addressline1,$addressline2,$town,$postcode,$town,$mobilephone);
+	$doupdate->bind_param("issssssss",$userid,$forename,$surname,$addressline1,$addressline2,$town,$postcode,$telephone,$mobilephone);
 	$doupdate->execute();
 	$doupdate->close();
 	$db->close();
