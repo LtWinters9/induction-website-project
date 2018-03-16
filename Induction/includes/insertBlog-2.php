53c91d418edfd31ff6af29c482f3e3975f39868b
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-16 15:47:07 +0000

    blog two option
    Second option for extened blog system

---
+++ Induction/includes/insertBlog-2.php
@@ -0,0 +1,53 @@
+<div class="row">
+  <div class="col-md-9 col-lg-9">
+    <h3><span class="heading">MAIN Heading </span></h3>
+    <article class="blog-item"><img src="assets/img/logo.jpg" class="pull-left img-responsive">
+      <div class="text">
+        <h3><a href="#">Lorem ipsum dolor sit consectetuer adipiscing elit. Donec odio. </a></h3>
+        <p>Lorem ipsum dolor sit amet </p>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus. Nullam malesuada erat ut turpis.Praesent dapibus,
+          neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus. </p>
+        <h4><a href="#">Learn more... </a></h4>
+      </div>
+      <div class="clearfix"></div>
+    </article>
+    <article class="blog-item"><img src="assets/img/logo.jpg" class="pull-left img-responsive">
+      <div class="text">
+        <h3><a href="#">Lorem ipsum dolor sit consectetuer adipiscing elit. Donec odio. </a></h3>
+        <p>Lorem ipsum dolor sit amet </p>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus. Nullam malesuada erat ut turpis.Praesent dapibus,
+          neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus. </p>
+        <h4><a href="#">Learn more... </a></h4>
+      </div>
+      <div class="clearfix"></div>
+    </article>
+    <article class="blog-item"><img src="assets/img/logo.jpg" class="pull-left img-responsive">
+      <div class="text">
+        <h3><a href="#">Lorem ipsum dolor sit consectetuer adipiscing elit. Donec odio. </a></h3>
+        <p>Lorem ipsum dolor sit amet </p>
+        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus. Nullam malesuada erat ut turpis.Praesent dapibus,
+          neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus. </p>
+        <h4><a href="#">Learn more... </a></h4>
+      </div>
+      <div class="clearfix"></div>
+    </article>
+  </div>
+  <div class="col-md-3 col-lg-3">
+    <h3> <span class="heading">Subheading </span></h3>
+    <div class="blog-item small-blog-item"><img src="assets/img/brahan.jpg" class="img-responsive">
+      <h4><a href="#">Donec odio. Quisque volutpat mattis eros. </a></h4>
+      <p>Lorem ipsum dolor sit amet </p>
+    </div>
+    <div class="blog-item small-blog-item"><img src="assets/img/brahan.jpg" class="img-responsive">
+      <h4><a href="#">Donec odio. Quisque volutpat mattis eros. </a></h4>
+      <p>Lorem ipsum dolor sit amet </p>
+    </div>
+    <div class="blog-item small-blog-item"><img src="assets/img/brahan.jpg" class="img-responsive">
+      <h4><a href="#">Donec odio. Quisque volutpat mattis eros. </a></h4>
+      <p>Lorem ipsum dolor sit amet </p>
+    </div>
+    <div class="blog-item small-blog-item"><img src="assets/img/brahan.jpg" class="img-responsive">
+      <h4><a href="#">Donec odio. Quisque volutpat mattis eros. </a></h4>
+      <p>Lorem ipsum dolor sit amet </p>
+    </div><a href="#" class="btn btn-default btn-block btn-lg" style="background-color:rgb(134,64,223);">Load more ...</a></div>
+</div>
