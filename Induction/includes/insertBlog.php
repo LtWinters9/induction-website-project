eeac4758c0477d7593b75aa9efc0c159c95f8835
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-16 14:53:54 +0000

    build article file

---
+++ Induction/includes/insertBlog.php
@@ -0,0 +1,24 @@
+ <div></div>
+<ol class="breadcrumb">
+  <li class="breadcrumb-item"><a><span>Forum </span></a></li>
+  <li class="breadcrumb-item"><a><span>General Discussions</span></a></li>
+  <li class="breadcrumb-item"><a><span> </span></a></li>
+</ol>
+
+
+<div></div>
+<div class="highlight-phone" style="background-color:#9da9ae;">
+  <h3 class="text-left" style="background-position:center;font-family:'Source Sans Pro', sans-serif;">Main Category</h3>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        <div class="intro">
+          <h2 style="font-family:'Source Sans Pro', sans-serif;"> <i class="fa fa-question"></i> Frequently asked questions</h2>
+          <p style="font-family:'Source Sans Pro', sans-serif;">Category information </p><a class="btn btn-primary" role="button" href="#" style="background-color:#7e3ca6;font-family:'Source Sans Pro', sans-serif;">View </a></div>
+      </div>
+      <div class="col-sm-4" style="background-image:url(&quot;assets/img/logo.jpg&quot;);background-repeat:no-repeat;">
+        <div class="d-none d-md-block iphone-mockup"></div>
+      </div>
+    </div>
+  </div>
+</div>
