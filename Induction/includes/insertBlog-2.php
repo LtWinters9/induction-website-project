c4e4e7b8e5bbef32a646101bb6adfa219f69c606
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-16 15:50:17 +0000

    ../

--- Induction/includes/insertBlog-2.php
+++ Induction/includes/insertBlog-2.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="col-md-9 col-lg-9">
     <h3><span class="heading">MAIN Heading </span></h3>
-    <article class="blog-item"><img src="assets/img/logo.jpg" class="pull-left img-responsive">
+    <article class="blog-item"><img src="../assets/img/logo.jpg" class="pull-left img-responsive">
       <div class="text">
         <h3><a href="#">Lorem ipsum dolor sit consectetuer adipiscing elit. Donec odio. </a></h3>
         <p>Lorem ipsum dolor sit amet </p>
@@ -11,7 +11,7 @@
       </div>
       <div class="clearfix"></div>
     </article>
-    <article class="blog-item"><img src="assets/img/logo.jpg" class="pull-left img-responsive">
+    <article class="blog-item"><img src="../assets/img/logo.jpg" class="pull-left img-responsive">
       <div class="text">
         <h3><a href="#">Lorem ipsum dolor sit consectetuer adipiscing elit. Donec odio. </a></h3>
         <p>Lorem ipsum dolor sit amet </p>
@@ -21,7 +21,7 @@
       </div>
       <div class="clearfix"></div>
     </article>
-    <article class="blog-item"><img src="assets/img/logo.jpg" class="pull-left img-responsive">
+    <article class="blog-item"><img src="../assets/img/logo.jpg" class="pull-left img-responsive">
       <div class="text">
         <h3><a href="#">Lorem ipsum dolor sit consectetuer adipiscing elit. Donec odio. </a></h3>
         <p>Lorem ipsum dolor sit amet </p>
@@ -34,19 +34,19 @@
   </div>
   <div class="col-md-3 col-lg-3">
     <h3> <span class="heading">Subheading </span></h3>
-    <div class="blog-item small-blog-item"><img src="assets/img/brahan.jpg" class="img-responsive">
+    <div class="blog-item small-blog-item"><img src="../assets/img/brahan.jpg" class="img-responsive">
       <h4><a href="#">Donec odio. Quisque volutpat mattis eros. </a></h4>
       <p>Lorem ipsum dolor sit amet </p>
     </div>
-    <div class="blog-item small-blog-item"><img src="assets/img/brahan.jpg" class="img-responsive">
+    <div class="blog-item small-blog-item"><img src="../assets/img/brahan.jpg" class="img-responsive">
       <h4><a href="#">Donec odio. Quisque volutpat mattis eros. </a></h4>
       <p>Lorem ipsum dolor sit amet </p>
     </div>
-    <div class="blog-item small-blog-item"><img src="assets/img/brahan.jpg" class="img-responsive">
+    <div class="blog-item small-blog-item"><img src="../assets/img/brahan.jpg" class="img-responsive">
       <h4><a href="#">Donec odio. Quisque volutpat mattis eros. </a></h4>
       <p>Lorem ipsum dolor sit amet </p>
     </div>
-    <div class="blog-item small-blog-item"><img src="assets/img/brahan.jpg" class="img-responsive">
+    <div class="blog-item small-blog-item"><img src="../assets/img/brahan.jpg" class="img-responsive">
       <h4><a href="#">Donec odio. Quisque volutpat mattis eros. </a></h4>
       <p>Lorem ipsum dolor sit amet </p>
     </div><a href="#" class="btn btn-default btn-block btn-lg" style="background-color:rgb(134,64,223);">Load more ...</a></div>
