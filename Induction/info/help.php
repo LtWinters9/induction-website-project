c76ca6bad7ca0f035cfd30710df8a5062e08f369
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-16 16:48:21 +0000

    -

--- Induction/info/help.php
+++ Induction/info/help.php
@@ -74,18 +74,7 @@ $collegeid=$_SESSION['collegeid'];
             </div>
         </div>
     </div>
-    <div class="newsletter-subscribe">
-        <div class="container" style="font-family:'Roboto Condensed', sans-serif;">
-            <div class="intro">
-                <h2 class="text-center">Subscribe for our Newsletter</h2>
-                <p class="text-center">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum nibh erat, pellentesque ut laoreet vitae. </p>
-            </div>
-            <form class="form-inline" method="post">
-                <div class="form-group"><input class="form-control" type="email" name="email" placeholder="Your Email"></div>
-                <div class="form-group"><button class="btn btn-primary" type="submit">Subscribe </button></div>
-            </form>
-        </div>
-          </div>
+    
         <div>
             <div class="container" style="font-family:'Roboto Condensed', sans-serif;">
                 <h1 style="font-family:'Roboto Condensed', sans-serif;font-size:28px;">How to Find Us<br><br></h1>
@@ -238,7 +227,6 @@ $collegeid=$_SESSION['collegeid'];
                 </div>
             </div>
         </div>
-    </div>
     <?php if($currentuser['userlevel']>1) {
      include "../includes/footer.php";
       } ?>
