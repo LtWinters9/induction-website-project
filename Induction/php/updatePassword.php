c55c56af8d4d588f3873f63112af82d6ded69aae
David Ferguson <Fergie_32@hotmail.com>
2018-03-29 10:49:37 +0100

    password files updated

--- Induction/php/updatePassword.php
+++ Induction/php/updatePassword.php
@@ -14,16 +14,25 @@ if(isset($userid)) {
 $db=createConnection();
 $userpass=$_POST['inputPassword'];
 $secondpass=$_POST['inputSecondPassword'];
+$userid = $_SESSION['userid'];
 
 $salt=getSalt(16);
 $cyphertext=makeHash($userpass,$salt,50);
 
-	$updatesql= "UPDATE userpass SET userpass='$userpass',salt='$cyphertext' WHERE passwordid='$passwordid'";
-    $doupdate=$db->prepare($updatesql);
-    $doupdate->bind_param("ssi",$userpass,$cyphertext,$passwordid);
-    $doupdate->execute();
-    $doupdate->close();
-		$db->close();
+    if(isset($_POST['inputPassword']) && trim($_POST['inputPassword'])!="") {
+        $plaintext=$_POST['inputPassword'];
+        //The following generates a salt which is 16 characters in length
+        $salt=getSalt(16);
+        //The following generates a hash using the plain text password, the salt and a work factor of 50
+        $hash=makeHash($plaintext,$salt,50);
+        //Now that salt and hash are generated they need stored to the table
+        $updatepasssql="update userpass set userpass=?,salt=? where userid=?";
+        $updatepass=$db->prepare($updatepasssql);
+        $updatepass->bind_param("ssi",$hash,$salt,$userid);
+        $updatepass->execute();
+        $updatepass->close();
+    }
+    $db->close();
 		header('Location: detailsUpdated.php');
 
 } else {
