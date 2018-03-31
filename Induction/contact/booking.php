1f6755ce028f76fbd5466c5edc45c7854ab15f11
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-03-31 21:41:55 +0100

    commit change

--- Induction/contact/booking.php
+++ Induction/contact/booking.php
@@ -27,7 +27,7 @@ $userid=checkUser($_SESSION['userid'],session_id(),2);
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Book now | UHI Induction Services</title>
-  
+
   <!-- bootstrap css libary -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -88,57 +88,57 @@ $result = $conn->query("select bookingid, bookdate, booktime, advisor, booked fr
 ?>
 
 
-        <div class="jumbotron-contact jumbotron-contact-sm">
-          <div class="container">
-            <div class="row">
-              <div class="col-sm-12 col-lg-12">
-                  <h1 class="h1">
-                      Book an Appointment with your Personal Academic Tutor (PAT)</h1>
-                <h1 class="h1">
-                Please select an appointment <small>to book now</small></h1>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div class="jumbotron-contact jumbotron-contact-sm">
         <div class="container">
           <div class="row">
-            <div class="col-md-8">
-              <div class="well well-sm">
-                <form action="../php/confirmBooking.php" method="post">
-                  <div class="row">
-                    <div class="col-md-6">
-                      <div class="form-group">
-                        <label for="subject">
-                            Select appointment</label>
-
-                        <?php
-                        echo "<select name = 'optionselected'>";
-                        while($row = mysqli_fetch_array($result)) {
-                            echo "<option value = ",$row[bookingid],">",
-                            " Date: ", $row[bookdate],
-                            " Time: ", $row[booktime],
-                            " Personal Academic Tutor: ", $row[advisor],"</option>";
-                        }
-                        echo "</select>";
-
-
-                        ?>
-                      </div>
-                    </div>
-                  </div>
-              </div>
+            <div class="col-sm-12 col-lg-12">
+              <h1 class="h1">
+                      Book an Appointment with your Personal Academic Tutor (PAT)</h1>
+              <h1 class="h1">
+                Please select an appointment <small>to book now</small></h1>
             </div>
           </div>
         </div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <div class="well well-sm">
+              <form action="../php/confirmBooking.php" method="post">
+                <div class="row">
+                  <div class="col-md-6">
+                    <div class="form-group">
+                      <label for="subject">
+                          Select appointment</label>
 
-        <div class="col-md-12">
-            <button type="submit" class="btn btn-success" id="btnBookNow">Confirm booking </button>
+<?php
+echo "<select name = 'optionselected'>";
+while($row = mysqli_fetch_array($result))
+{
+  echo "<option value = ",$row[bookingid],">",
+  " Date: ", $row[bookdate],
+  " Time: ", $row[booktime],
+  " Personal Academic Tutor: ", $row[advisor],"</option>";
+}
+
+echo "</select>";
+?>
+                    </div>
+                  </div>
+                </div>
+            </div>
+          </div>
         </div>
-        </form>
+      </div>
+
+      <div class="col-md-12">
+        <button type="submit" class="btn btn-success" id="btnBookNow">Confirm booking </button>
+      </div>
+      </form>
 
 
 
-        <?php
+      <?php
 
 $conn->close();
 ?>
@@ -150,62 +150,61 @@ $conn->close();
 
 
 
-        <div class="testimonials-clean"></div>
-        <div data-aos="fade-right" data-aos-once="true" class="highlight-clean">
-          <div class="container">
-            <div class="intro">
-              <h2 class="text-center" data-aos="fade-left" data-aos-duration="800" data-aos-delay="100" data-aos-once="true" style="font-family:'Roboto Condensed', sans-serif;">Questions about your student experience?</h2>
-            </div>
-            <div class="buttons"><a class="btn btn-light" role="button" href="../account/login.php" data-aos="fade-up" data-aos-duration="900" data-aos-delay="200" data-aos-once="true" style="font-family:'Roboto Condensed', sans-serif;background-color:#7e3ca6;color:#ffffff;">CHAT NOW</a></div>
+      <div class="testimonials-clean"></div>
+      <div data-aos="fade-right" data-aos-once="true" class="highlight-clean">
+        <div class="container">
+          <div class="intro">
+            <h2 class="text-center" data-aos="fade-left" data-aos-duration="800" data-aos-delay="100" data-aos-once="true" style="font-family:'Roboto Condensed', sans-serif;">Questions about your student experience?</h2>
           </div>
+          <div class="buttons"><a class="btn btn-light" role="button" href="../account/login.php" data-aos="fade-up" data-aos-duration="900" data-aos-delay="200" data-aos-once="true" style="font-family:'Roboto Condensed', sans-serif;background-color:#7e3ca6;color:#ffffff;">CHAT NOW</a></div>
         </div>
-        <div class="testimonials-clean"></div>
-        <div class="footer-dark" style="background-color:#7e3ca6;">
-          <footer>
-            <div class="container">
-              <div class="row">
-                <div class="col-md-6 item text">
-                  <h3 class="text-uppercase" style="color:#ffffff;font-family:'Roboto Condensed', sans-serif;">The University of the Highlands and Islands</h3>
-                  <p><a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">About</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">News and Events</a> •
-                    <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">Business and employers</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">Vacancies</a>                    • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">Give to UHI</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">FOI</a> •
-                    <a
-                      href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">Cookies</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">Contact Us</a></p>
-                </div>
-                <div class="col-sm-4 col-md-3 item">
-                  <h3 class="text-uppercase" style="color:#ffffff;font-family:'Roboto Condensed', sans-serif;">About UHI</h3>
-                  <ul>
-                    <li><a href="#" style="color:#ffffff;">Argyll College UHI </a></li>
-                    <li><a href="#">Highland Theological College UHI </a></li>
-                    <li><a href="#">Inverness College UHI </a></li>
-                    <li><a href="https://www.lews.uhi.ac.uk/" target="_blank">Lews Castle College UHI</a> </li>
-                    <li><a href="https://www.moray.uhi.ac.uk/" target="_blank">Moray College UHI</a> </li>
-                    <li><a href="https://www.nafc.uhi.ac.uk/" target="_blank">NAFC Marine Centre UHI</a> </li>
-                    <li><a href="https://www.northhighland.uhi.ac.uk/" target="_blank">North Highland College UHI</a> </li>
-                    <li><a href="https://www.orkney.uhi.ac.uk/" target="_blank">Orkney College UHI</a> </li>
-                  </ul>
-                </div>
-                <div class="col-sm-4 col-md-3 item">
-                  <h3 class="text-uppercase" style="color:#ffffff;font-family:'Roboto Condensed', sans-serif;">Information </h3>
-                  <ul>
-                    <li><a href="https://www.uhi.ac.uk/en/" target="_blank">Perth College UHI </a></li>
-                    <li><a href="../info/opening-times.php">Opening Times</a></li>
-                    <li><a href="../info/how-to-find-us/php" style="color:#ffffff;">How To Find Us</a></li>
-                    <li><a href="../info/sitemap.php">Sitemap</a></li>
-                  </ul>
-                </div>
-                <div class="col-sm-4 col-md-12 col-xl-12 offset-xl-0 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a></div>
+      </div>
+      <div class="testimonials-clean"></div>
+      <div class="footer-dark" style="background-color:#7e3ca6;">
+        <footer>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6 item text">
+                <h3 class="text-uppercase" style="color:#ffffff;font-family:'Roboto Condensed', sans-serif;">The University of the Highlands and Islands</h3>
+                <p><a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">About</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">News and Events</a> •
+                  <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">Business and employers</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">Vacancies</a>                  • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">Give to UHI</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">FOI</a> •
+                  <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">Cookies</a> • <a href="https://www.uhi.ac.uk/en/cookies-terms-conditions/" target="_blank" style="color:#ffffff;">Contact Us</a></p>
               </div>
-              <p class="copyright" style="color:#ffffff;">© University of the Highlands and IslandsRegistered office: 12b Ness Walk, Inverness, Scotland, IV3 5SQ&nbsp; </p>
+              <div class="col-sm-4 col-md-3 item">
+                <h3 class="text-uppercase" style="color:#ffffff;font-family:'Roboto Condensed', sans-serif;">About UHI</h3>
+                <ul>
+                  <li><a href="#" style="color:#ffffff;">Argyll College UHI </a></li>
+                  <li><a href="#">Highland Theological College UHI </a></li>
+                  <li><a href="#">Inverness College UHI </a></li>
+                  <li><a href="https://www.lews.uhi.ac.uk/" target="_blank">Lews Castle College UHI</a> </li>
+                  <li><a href="https://www.moray.uhi.ac.uk/" target="_blank">Moray College UHI</a> </li>
+                  <li><a href="https://www.nafc.uhi.ac.uk/" target="_blank">NAFC Marine Centre UHI</a> </li>
+                  <li><a href="https://www.northhighland.uhi.ac.uk/" target="_blank">North Highland College UHI</a> </li>
+                  <li><a href="https://www.orkney.uhi.ac.uk/" target="_blank">Orkney College UHI</a> </li>
+                </ul>
+              </div>
+              <div class="col-sm-4 col-md-3 item">
+                <h3 class="text-uppercase" style="color:#ffffff;font-family:'Roboto Condensed', sans-serif;">Information </h3>
+                <ul>
+                  <li><a href="https://www.uhi.ac.uk/en/" target="_blank">Perth College UHI </a></li>
+                  <li><a href="../info/opening-times.php">Opening Times</a></li>
+                  <li><a href="../info/how-to-find-us/php" style="color:#ffffff;">How To Find Us</a></li>
+                  <li><a href="../info/sitemap.php">Sitemap</a></li>
+                </ul>
+              </div>
+              <div class="col-sm-4 col-md-12 col-xl-12 offset-xl-0 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a></div>
             </div>
-          </footer>
-        </div>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
-        <script src="../dist/js/contact_me.js"></script>
-        <script src="../dist/js/jqBootstrapValidation.js"></script>
-        <script src="../assets/js/script.min.js"></script>
-        <script src="../dist/js/cookies.js"></script>
+            <p class="copyright" style="color:#ffffff;">© University of the Highlands and IslandsRegistered office: 12b Ness Walk, Inverness, Scotland, IV3 5SQ&nbsp; </p>
+          </div>
+        </footer>
+      </div>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta.2/js/bootstrap.bundle.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
+      <script src="../dist/js/contact_me.js"></script>
+      <script src="../dist/js/jqBootstrapValidation.js"></script>
+      <script src="../assets/js/script.min.js"></script>
+      <script src="../dist/js/cookies.js"></script>
 
   </body>
 
