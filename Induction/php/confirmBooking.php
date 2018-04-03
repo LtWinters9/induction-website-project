6ff467960d4d74bb4588036bca76c6f610d6ee48
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-04-03 11:24:41 +0100

    add security check

--- Induction/php/confirmBooking.php
+++ Induction/php/confirmBooking.php
@@ -81,9 +81,15 @@ $insertorder->close();
 
 <body>
 
+  <?php if($currentuser['userlevel']>0) {
+   header("location: ../account/login");
+   } ?>
+
 <?php if($currentuser['userlevel']>1) {
     include "../includes/navLevel2.php";
 } ?>
+
+
 
 <!-- Book Section -->
 <div class="highlight-blue">
