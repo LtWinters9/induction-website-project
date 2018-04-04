0b022dfe76ce978b6183907a4d8d9da133ca7260
btraill20 <35263705+btraill20@users.noreply.github.com>
2018-04-04 13:50:19 +0100

    API file upload
    an api for uploading new files, since hard coding it would require me to do stuff within a server. so this should bypass that need.

---
+++ Induction/php/processfile.php
@@ -0,0 +1,29 @@
+<?php
+require_once '../API/google-api-php-client/src/Google/autoload.php';
+
+putenv('GOOGLE_APPLICATION_CREDENTIALS=/path/to/service/account/key.json');
+$client = new Google_Client();
+$client->addScope(Google_Service_Drive::DRIVE);
+$client->useApplicationDefaultCredentials();
+
+$service = new Google_Service_Drive($client);
+
+$fileId = "0Bxxxxxxxxxxxxxxxxxxxx"; // Google File ID
+$content = $service->files->get($fileId, array("alt" => "media"));
+
+// Open file handle for output.
+
+$outHandle = fopen("/path/to/destination", "w+");
+
+// Until we have reached the EOF, read 1024 bytes at a time and write to the output file handle.
+
+while (!$content->getBody()->eof()) {
+        fwrite($outHandle, $content->getBody()->read(1024));
+}
+
+// Close output file handle.
+
+fclose($outHandle);
+echo "Done.\n"
+
+?>
