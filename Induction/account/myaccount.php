a279f75a4134db8e95b22cd2092ec7a5a98a89b8
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-04-09 17:34:45 +0100

    test error

--- Induction/account/myaccount.php
+++ Induction/account/myaccount.php
@@ -86,7 +86,7 @@ $userid=checkUser($_SESSION['userid'],session_id(),2);
                   <div class="col-sm-6 col-md-5 col-lg-4 item">
                       <div data-aos="zoom-in-down" data-aos-duration="300" data-aos-delay="300" data-aos-once="true" class="box"><i class="fa fa-file-zip-o icon" style="color:#7e3ca6;"></i>
                           <h3 class="name" style="font-family:'Roboto Condensed', sans-serif;">Forms and Guidance </h3>
-                          <p class="text-center description" style="font-family:'Roboto Condensed', sans-serif;font-size:16px;">Browse though the files that are related to your current course. &nbsp;This will ensure you're ready for your first day, or stay ontop of your current course work with updates.</p><a href="../course/student-course" class="learn-more" style="font-family:'Roboto Condensed', sans-serif;">Browse now »</a></div>
+                          <p class="text-center description" style="font-family:'Roboto Condensed', sans-serif;font-size:16px;">Browse though the files that are related to your current course. &nbsp;This will ensure you're ready for your first day, or stay ontop of your current course work with updates.</p><a href="../error_docs/maintenance.html" class="learn-more" style="font-family:'Roboto Condensed', sans-serif;">Browse now »</a></div>
                   </div>
                   <div class="col-sm-6 col-md-5 col-lg-4 item">
                       <div data-aos="zoom-in-down" data-aos-duration="400" data-aos-delay="400" data-aos-once="true" class="box"><i class="icon ion-images icon" style="color:#7e3ca6;"></i>
@@ -129,7 +129,7 @@ $userid=checkUser($_SESSION['userid'],session_id(),2);
         } ?>
 
       <script src="../dist/scripts/inductioncorejs.js"></script>
-    
+
 
 </body>
 </html>
