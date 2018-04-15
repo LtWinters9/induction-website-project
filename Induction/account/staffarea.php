b6fc56616d00d37527a86f94bac3c36ce808d63a
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-04-15 17:01:18 +0100

    update staff

--- Induction/account/staffarea.php
+++ Induction/account/staffarea.php
@@ -22,7 +22,7 @@ $userid=checkUser($_SESSION['userid'],session_id(),2);
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to your account, <?php echo $forename; ?>  | UHI Induction Services</title>
+    <title>Welcome to your Staff area, <?php echo $forename; ?>  | UHI Induction Services</title>
 
     <!-- bootstrap css libary -->
     <link rel="stylesheet" href="../dist/styles/main/css-main.css">
@@ -46,156 +46,166 @@ $userid=checkUser($_SESSION['userid'],session_id(),2);
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="../dist/favicons.ico/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
-
-    <!-- Cookies -->
-    <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
-    <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
-
 </head>
 
 <body>
 
-  <?php if($currentuser['userlevel']>2) {
+  <?php if($currentuser['userlevel']>1) {
     include "../includes/navLevel3.php";
      } ?>
 
 
-<header id="header">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-10">
-        <h1><span class="glyphicon glyphicon-cog" aria-hidden="true"></span> Dashboard <small>Manage Your Site</small></h1>
-      </div>
-      <div class="col-md-2">
-        <div class="dropdown">
-          <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Create Content
-  <span class="caret"></span></button>
-          <ul class="dropdown-menu">
-            <li><a href="#">Add Pages</a></li>
-            <li><a href="#">Add Posts</a></li>
-            <li><a href="#">Add Users</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
-</header>
-<br>
+     <div id="wrapper">
+         <nav class="navbar header-top fixed-top navbar-expand-lg  navbar-dark bg-dark">
+           <a class="navbar-brand" href="#">LOGO</a>
+           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText"
+             aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+           </button>
+           <div class="collapse navbar-collapse" id="navbarText">
+             <ul class="navbar-nav side-nav">
+               <li class="nav-item">
+                 <a class="nav-link" href="#">Home
+                   <span class="sr-only">(current)</span>
+                 </a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="#">Side Menu Items</a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="#">Pricing</a>
+               </li>
+             </ul>
+             <ul class="navbar-nav ml-md-auto d-md-flex">
+               <li class="nav-item">
+                 <a class="nav-link" href="#">Home
+                   <span class="sr-only">(current)</span>
+                 </a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="#">Top Menu Items</a>
+               </li>
+               <li class="nav-item">
+                 <a class="nav-link" href="#">Pricing</a>
+               </li>
+             </ul>
+           </div>
+         </nav>
+         <div class="container-fluid">
+                 <div class="row">
+             <div class="col">
+               <div class="card">
+                 <div class="card-body">
+                   <h5 class="card-title">Try Other</h5>
+                   <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6>
+                   <p class="card-text">You can also try different version of Bootstrap V4 side menu. Click below link to view all Bootstrap Menu versions.</p>
+                   <a href="https://bootsnipp.com/pradeep330" class="card-link">link</a>
+                   <a href="http://websitedesigntamilnadu.com" class="card-link">Another link</a>
+                 </div>
+               </div>
+             </div>
+             <div class="col">
+               <div class="card">
+                 <div class="card-body">
+                   <h5 class="card-title">Try Other</h5>
+                   <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6>
+                   <p class="card-text">You can also try different version of Bootstrap V4 side menu. Click below link to view all Bootstrap Menu versions.</p>
+                   <a href="https://bootsnipp.com/pradeep330" class="card-link">link</a>
+                   <a href="http://websitedesigntamilnadu.com" class="card-link">Another link</a>
+                 </div>
+               </div>
+             </div>
+             <div class="col">
+               <div class="card">
+                 <div class="card-body">
+                   <h5 class="card-title">Try Other</h5>
+                   <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6>
+                   <p class="card-text">You can also try different version of Bootstrap V4 side menu. Click below link to view all Bootstrap Menu versions.</p>
+                   <a href="https://bootsnipp.com/pradeep330" class="card-link">link</a>
+                   <a href="http://websitedesigntamilnadu.com" class="card-link">Another link</a>
+                 </div>
+               </div>
+             </div>
+             <div class="col">
+               <div class="card">
+                 <div class="card-body">
+                   <h5 class="card-title">Try Other</h5>
+                   <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6>
+                   <p class="card-text">You can also try different version of Bootstrap V4 side menu. Click below link to view all Bootstrap Menu versions.</p>
+                   <a href="https://bootsnipp.com/pradeep330" class="card-link">link</a>
+                   <a href="http://websitedesigntamilnadu.com" class="card-link">Another link</a>
+                 </div>
+               </div>
+             </div>
+             <div class="col">
+               <div class="card">
+                 <div class="card-body">
+                   <h5 class="card-title">Try Other</h5>
+                   <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6>
+                   <p class="card-text">You can also try different version of Bootstrap V4 side menu. Click below link to view all Bootstrap Menu versions.</p>
+                   <a href="https://bootsnipp.com/pradeep330" class="card-link">link</a>
+                   <a href="http://websitedesigntamilnadu.com" class="card-link">Another link</a>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div class="row">
+             <div class="col">
+               <div class="card">
+                 <div class="card-body">
+                   <h5 class="card-title">Try Other</h5>
+                   <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 Snippet by pradeep330</h6>
+                   <p class="card-text">You can also try different version of Bootstrap V4 side menu. Click below link to view all Bootstrap Menu versions.</p>
+                   <a href="https://bootsnipp.com/pradeep330" class="card-link">link</a>
+                   <a href="http://websitedesigntamilnadu.com" class="card-link">Another link</a>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <div class="row">
+             <div class="col">
+               <div class="card">
+                 <div class="card-body">
+                   <h5 class="card-title">Card title</h5>
+                   <table class="table">
+       <thead>
+         <tr>
+           <th scope="col">#</th>
+           <th scope="col">First</th>
+           <th scope="col">Last</th>
+           <th scope="col">Handle</th>
+         </tr>
+       </thead>
+       <tbody>
+         <tr>
+           <th scope="row">1</th>
+           <td>Mark</td>
+           <td>Otto</td>
+           <td>@mdo</td>
+         </tr>
+         <tr>
+           <th scope="row">2</th>
+           <td>Jacob</td>
+           <td>Thornton</td>
+           <td>@fat</td>
+         </tr>
+         <tr>
+           <th scope="row">3</th>
+           <td>Larry</td>
+           <td>the Bird</td>
+           <td>@twitter</td>
+         </tr>
+       </tbody>
+     </table>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
 
-<section id="breadcrumb">
-  <div class="container">
-    <ol class="breadcrumb">
-      <li class="active">Dashboard</li>
-    </ol>
-  </div>
-</section>
 
-
-<section id="main">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-3">
-        <div class="list-group">
-          <a href="index.html" class="list-group-item active main-color-bg"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
-        Dashboard <span class="badge">12</span>
-      </a>
-          <a href="pages.html" class="list-group-item"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> Pages<span class="badge">25</span></a>
-          <a href="posts.html" class="list-group-item"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Posts<span class="badge">126</span></a>
-          <a href="users.html" class="list-group-item"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Users <span class="badge">12</span></a>
-        </div>
-
-        <div class="well">
-          <h4>Disk Space Used</h4>
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
-              60%
-            </div>
-          </div>
-          <h4>Bandwidth Used</h4>
-          <div class="progress">
-            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">
-              40%
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="col-md-9">
-        <div class="panel panel-default">
-          <div class="panel-heading" style="background-color:  #095f59;">
-            <h3 class="panel-title">Website Overview</h3>
-          </div>
-          <div class="panel-body">
-            <div class="col-md-3">
-              <div class="well dash-box">
-                <h2><span class="glyphicon glyphicon-user" aria-hidden="true"></span> 12</h2>
-                <h4>Users</h4>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="well dash-box">
-                <h2><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> 25</h2>
-                <h4>Pages</h4>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="well dash-box">
-                <h2><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>126</h2>
-                <h4>Posts</h4>
-              </div>
-            </div>
-            <div class="col-md-3">
-              <div class="well dash-box">
-                <h2><span class="glyphicon glyphicon-stats" aria-hidden="true"></span> 2129</h2>
-                <h4>Visitores</h4>
-              </div>
-            </div>
-          </div>
-        </div>
-        <!--Latest User-->
-        <div class="panel panel-default">
-          <div class="panel-heading" style="background-color:  #095f59;>
-    <h3 class=" panel-title ">Latest Users</h3>
-  </div>
-  <div class="panel-body ">
-    <table class="table table-striped table-hover ">
-      <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Joined</th>
-      </tr>
-
-    <tr>
-      <td>Madhav Prasad</td>
-      <td>mr.madhavprasad@gmail.com</td>
-      <td>Dec 13,2014</td>
-    </tr>
-    <tr>
-      <td>Nagendra Kushwaha</td>
-      <td>nkushwaha822@gmail.com</td>
-      <td>Feb 15,2014</td>
-    </tr>
-    <tr>
-      <td>Arun Kumar</td>
-      <td>kumarun@yahoo.com</td>
-      <td>Aug 17, 2015</td>
-    </tr>
-    <tr>
-      <td>Nabin Singh</td>
-      <td>singhNavs@outlook.com</td>
-      <td>March 08,2016</td>
-    </tr>
-    </table>
-
-  </div>
-</div>
-
-      </div>
-    </div>
-  </div>
-</section>
-
-
-<?php if($currentuser['userlevel']>2) {
+<?php if($currentuser['userlevel']>1) {
  include "../includes/footer.php";
   } ?>
 
