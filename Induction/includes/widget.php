a0b2e3c8d7f14b1fa97033e33f6497a98e634cba
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-04-15 18:33:11 +0100

    add row

--- Induction/includes/widget.php
+++ Induction/includes/widget.php
@@ -37,11 +37,11 @@
      </div>
    </nav>
    <div class="container-fluid">
-     <!--<div class="row">
+     <div class="row">
        <div class="col">
          <div class="card">
            <div class="card-body">
-             <h5 class="card-title">Try Other</h5>
+             <h5 class="card-title">Test</h5>
              <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 </h6>
              <p class="card-text">You can also try different version of Bootstrap V4 side menu. Click below link to view all Bootstrap Menu versions.</p>
              <a href="https://bootsnipp.com/pradeep330" class="card-link">link</a>
@@ -49,19 +49,8 @@
            </div>
          </div>
        </div>
-       <div class="col">
-         <div class="card">
-           <div class="card-body">
-             <h5 class="card-title">Try Other</h5>
-             <h6 class="card-subtitle mb-2 text-muted">Bootstrap 4.0.0 </h6>
-             <p class="card-text">You can also try different version of Bootstrap V4 side menu. Click below link to view all Bootstrap Menu versions.</p>
-             <a href="#" class="card-link">link</a>
-             <a href="#" class="card-link">Another link</a>
-           </div>
-         </div>
-       </div>
      </div>
-     <div class="row"> -->
+     <div class="row">
        <div class="col">
          <div class="card">
            <div class="card-body">
