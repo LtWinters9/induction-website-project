5e7ad35b1150f7e1e928348bf4e3a705306ce200
btraill20 <35263705+btraill20@users.noreply.github.com>
2018-04-24 12:36:25 +0100

    filedownload

--- Induction/php/processfile.php
+++ Induction/php/processfile.php
@@ -1,11 +1,11 @@
 <?php
-require_once '../API/google-api-php-client/src/Google/autoload.php';
-putenv('GOOGLE_APPLICATION_CREDENTIALS=/path/to/service/account/key.json');
+//require_once '../API/google-api-php-client/src/Google/autoload.php';
+
 $client = new Google_Client();
 $client->addScope(Google_Service_Drive::DRIVE);
 $client->useApplicationDefaultCredentials();
 $service = new Google_Service_Drive($client);
-$fileId = "0B13NpNXk5vDNhEsj4I8vrrjN8lNuPZSc3A?usp=sharing"; // Google File ID
+$fileId = "https://drive.google.com/drive/folders/13NpNXk5vDNhEsj4I8vrrjN8lNuPZSc3A?usp=sharing"; // Google File ID
 $response = $driveService->files->get($fileId, array('alt' => 'media'));
 $content = $response->getBody()->getContents();
 ?>
