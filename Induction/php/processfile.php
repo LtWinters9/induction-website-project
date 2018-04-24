d63a1e45d8ceb36f0cb37c197de60918fdd45682
btraill20 <35263705+btraill20@users.noreply.github.com>
2018-04-24 12:32:51 +0100

    .

--- Induction/php/processfile.php
+++ Induction/php/processfile.php
@@ -1,29 +1,11 @@
 <?php
 require_once '../API/google-api-php-client/src/Google/autoload.php';
-
 putenv('GOOGLE_APPLICATION_CREDENTIALS=/path/to/service/account/key.json');
 $client = new Google_Client();
 $client->addScope(Google_Service_Drive::DRIVE);
 $client->useApplicationDefaultCredentials();
-
 $service = new Google_Service_Drive($client);
-
-$fileId = "https://drive.google.com/drive/folders/13NpNXk5vDNhEsj4I8vrrjN8lNuPZSc3A?usp=sharing"; // Google File ID
-$content = $service->files->get($fileId, array("alt" => "media"));
-
-// Open file handle for output.
-
-$outHandle = fopen("/path/to/destination", "w+");
-
-// Until we have reached the EOF, read 1024 bytes at a time and write to the output file handle.
-
-while (!$content->getBody()->eof()) {
-        fwrite($outHandle, $content->getBody()->read(1024));
-}
-
-// Close output file handle.
-
-fclose($outHandle);
-echo "Done.\n"
-
+$fileId = "0B13NpNXk5vDNhEsj4I8vrrjN8lNuPZSc3A?usp=sharing"; // Google File ID
+$response = $driveService->files->get($fileId, array('alt' => 'media'));
+$content = $response->getBody()->getContents();
 ?>
