6afa69e2b6c4dd7458773b1b6002716cd4a04de1
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-04-27 19:23:18 +0100

    Wrong nav location

--- Induction/contact/technical-support.php
+++ Induction/contact/technical-support.php
@@ -15,13 +15,6 @@ $userid=checkUser($_SESSION['userid'],session_id(),2);
 <!DOCTYPE html>
 <html lang="en">
 
-<?php if($currentuser['userlevel']==2) {
-    include "../includes/navLevel2.php";
-} ?>
-
-<?php if($currentuser['userlevel']==3) {
-    include "../includes/navLevel3.php";
-} ?>
 
 <head>
   <meta charset="utf-8">
@@ -37,10 +30,15 @@ $userid=checkUser($_SESSION['userid'],session_id(),2);
 </head>
 
 <body>
-  <?php if($currentuser['userlevel']>1) {
-    include "../includes/navLevel2.php";
-     } ?>
+  <!-- Nav Bar -->
+      <?php if($currentuser['userlevel']==2) {
+       include "../includes/navLevel2.php";
+        } ?>
 
+    <?php if($currentuser['userlevel']==3) {
+        include "../includes/navLevel3.php";
+    } ?>
+    
   <div class="features-boxed">
     <div class="container">
       <div class="container">
