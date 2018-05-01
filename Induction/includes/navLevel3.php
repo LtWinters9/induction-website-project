24f62e814147fd3817219ed8088897da91332586
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-05-01 12:11:29 +0100

    name

--- Induction/includes/navLevel3.php
+++ Induction/includes/navLevel3.php
@@ -5,7 +5,7 @@
       <ul class="nav navbar-nav nav-right">
         <li class="nav-item" role="presentation"><a class="nav-link" href="../web/index" style="font-family:'Roboto Condensed', sans-serif;color:#ffffff;"><i class="fa fa-home"></i>&nbsp;Current Students</a></li>
         <li class="dropdown"><a class="dropdown-toggle nav-link" data-toggle="dropdown" aria-expanded="false" href="#" style="font-family:'Roboto Condensed', sans-serif;color:#ffffff;"><i class="fa fa-user"></i>&nbsp;My Account</a>
-          <div class="dropdown-menu" role="menu" style="background-color:#7e3ca6;"><a class="dropdown-item" role="presentation" href="../blog/collegeblog" style="color:#ffffff;background-color:#7e3ca6;">UHI Form </a>
+          <div class="dropdown-menu" role="menu" style="background-color:#7e3ca6;"><a class="dropdown-item" role="presentation" href="../blog/collegeblog" style="color:#ffffff;background-color:#7e3ca6;">UHI Forum </a>
             <a class="dropdown-item" role="presentation" href="../account/updateuserselect" style="color:#ffffff;background-color:#7e3ca6;">Edit User</a>
             <a class="dropdown-item" role="presentation" href="../info/help" style="color:#ffffff;background-color:#7e3ca6;">Help Centre</a>
             <a class="dropdown-item" role="presentation" href="../php/logout" style="color:#ffffff;background-color:#7e3ca6;">Logout</a>
