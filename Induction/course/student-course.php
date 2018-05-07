1aa29154266c3cd2a0db2c5a5582cb91115141c2
LtWinters9 <35262595+LtWinters9@users.noreply.github.com>
2018-05-07 20:02:05 +0100

    download cloud file

--- Induction/course/student-course.php
+++ Induction/course/student-course.php
@@ -118,7 +118,7 @@ $userid=checkUser($_SESSION['userid'],session_id(),2);
           <div id="item-1-4" class="tab-pane fade" role="tabpanel" aria-labelledby="item-1-4-tab">
             <!-- File Downloads -->
             <h4><i class="ion-android-cloud icon"></i> File Downloads</h4>
-            <p style="color:#000000;"><i class="ion-android-cloud"></i> <a href="../php/processfile.php"> Cloud Computing (1MB)</a>
+            <p style="color:#000000;"><i class="ion-android-cloud"></i> <a href="https://docs.google.com/document/d/1vFOPRb-cXQUVmOCz7bIUcP06HWaGFatLKddYtMQFtmE/edit?usp=sharing" target="_blank" >Cloud Computing (1MB)</a>
               <?php echo $modulename; ?> </p>
             <p style="color:#000000;"><i class="ion-android-cloud"></i> <a href="../php/uploadform.php">Databases (1MB)</a>
               <?php echo $modulename; ?> </p>
